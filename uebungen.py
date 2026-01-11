be73e46da2e1859561d40d8d31037af22b7b8ae2
Sadamichi Kato <sadamichikato@gmail.com>
2026-01-11 14:56:16 +0900

    meine erste

---
+++ uebungen.py
@@ -0,0 +1,8 @@
+singers = ["Taylor", "Ed", "Ronan"]
+songs = ("Change","Old Phone","Guiding Light")
+pares = {"Taylor":"Change", "Ed":"Old Phone","Ronan":"Guiding Light"}
+print("Taylor" in pares)
+
+print("Taylor" in singers)
+
+print("Taylor" in songs)
