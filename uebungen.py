9cfbcc935cea9ad19443f8c81cec8ff17af4ff13
Sadamichi Kato <sadamichikato@gmail.com>
2026-01-14 15:52:09 +0900

    removing the file

--- uebungen.py
+++
@@ -1,8 +0,0 @@
-singers = ["Taylor", "Ed", "Ronan"]
-songs = ("Change","Old Phone","Guiding Light")
-pares = {"Taylor":"Change", "Ed":"Old Phone","Ronan":"Guiding Light"}
-print("Taylor" in pares)
-
-print("Taylor" in singers)
-
-print("Taylor" in songs)
