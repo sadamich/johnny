778e047c5f557ca530f72bbc2b606e50b9edc98e
Sadamichi Kato <sadamichikato@gmail.com>
2026-01-14 17:56:21 +0900

    up date

---
+++ uebungen.py
@@ -0,0 +1,18 @@
+singers = ["Taylor", "Ed", "Ronan"]
+songs = ("Change", "Old Phone", "Guiding Light")
+pares = {"Taylor":"Change","Ed":"Old Phone","Ronan":"Guding Light"}
+
+
+
+print("Taylor" in pares)
+
+
+print("Taylor" in singers)
+
+
+print("Taylor" in songs)
+
+
+print("Ed" in songs)
+
+         
