47e7ceb90dca3a089c1f1c85f7365f43b479e94f
sadamich <sadamichikato@gmail.com>
2026-01-22 16:22:41 +0900

    ML and Optimization

---
+++ ml_estimation_optimization.R
@@ -0,0 +1,10 @@
+### The maxLik function ###
+library(maxLik)
+x<- rnorm(100)
+loglik<- function(theta){
+mu <- theta[1]
+sigma <- theta[2]
+sum(dnorm(x, mean=mu, sd = sigma, log=TRUE))
+}
+m<- maxLik(loglik, start = c(mu=1, sigma=2))
+summary(m)
