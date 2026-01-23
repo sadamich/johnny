d3eb8939b6b8b6ba2f143c23a3893f352d0ea8da
sadamich <sadamichikato@gmail.com>
2026-01-23 14:09:41 +0900

    trigonometric

---
+++ trigonometric_function.R
@@ -0,0 +1,19 @@
+### Trigonometric function ###
+
+sin(pi/6)
+cos(pi/6)
+tan(pi/6)
+
+sin(pi/4)
+cos(pi/4)
+tan(pi/4)
+
+sin(pi/3)
+cos(pi/3)
+tan(pi/3)
+
+sin(pi/6)^2+cos(pi/6)^2
+sin(pi/4)^2+cos(pi/4)^2
+sin(pi/3)^2+cos(pi/3)^2
+
+curve(cos(x), -10,10, lty =2 , ylim=c(-1.5,1.5), xlab= "x", yaxs ="i", ylab = "cos(x)")
