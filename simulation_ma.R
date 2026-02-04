5c9c3a73915e609abb80c34b64f8d7661bf4001e
sadamich <sadamichikato@gmail.com>
2026-02-04 15:11:29 +0900

    MA Model

--- simulation_ma.R
+++ simulation_ma.R
@@ -40,3 +40,4 @@ grid.arrange(p1, p2, ncol = 1)
 ### Source:Christiaan Heij, Paul de Boer, Philip Hans Franses, Teun Kloek, ###
 ### Herman K. van Dijk (2004).Econometric Methods with Applications in     ###
 ### Business and Economics. Oxford University Press                        ###
+### https://global.oup.com/booksites/content/0199268010/                   ###
