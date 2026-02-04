2047fbd260f8e280b63d8c34fa2ec9eece700af4
sadamich <sadamichikato@gmail.com>
2026-02-04 15:21:16 +0900

    VECM

--- vecm_comparison.R
+++ vecm_comparison.R
@@ -2,6 +2,7 @@
 ### Source:Christiaan Heij, Paul de Boer, Philip Hans Franses, Teun Kloek, ###
 ### Herman K. van Dijk (2004).Econometric Methods with Applications in     ###
 ### Business and Economics. Oxford University Press                        ###
+### https://global.oup.com/booksites/content/0199268010/                   ###
 xm722<- read.csv("xm722.csv", header = TRUE)
 str(xm722)
 attach(xm722)
