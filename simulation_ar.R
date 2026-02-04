5678659158d93412faeaeb46db9a297fff60709e
sadamich <sadamichikato@gmail.com>
2026-02-04 15:08:35 +0900

    AR Model

--- simulation_ar.R
+++ simulation_ar.R
@@ -69,3 +69,4 @@ grid.arrange(p1, p3, p2, p4, ncol = 2)
 ### Source:Christiaan Heij, Paul de Boer, Philip Hans Franses, Teun Kloek, ###
 ### Herman K. van Dijk (2004).Econometric Methods with Applications in     ###
 ### Business and Economics. Oxford University Press                        ###
+### https://global.oup.com/booksites/content/0199268010/                   ###
