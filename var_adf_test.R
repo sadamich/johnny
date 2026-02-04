f1edfa67c429c389ed98ad7e5fe1decdf4a6f12b
sadamich <sadamichikato@gmail.com>
2026-02-04 15:14:35 +0900

    ADF Test

--- var_adf_test.R
+++ var_adf_test.R
@@ -1,7 +1,7 @@
 ### Source:Christiaan Heij, Paul de Boer, Philip Hans Franses, Teun Kloek, ###
 ### Herman K. van Dijk (2004).Econometric Methods with Applications in     ###
 ### Business and Economics. Oxford University Press, p.676                 ###
-### https://global.oup.com/booksites/content/0199268010/                                ###
+### https://global.oup.com/booksites/content/0199268010/                   ###
 ### Example 7 27 Interest and bond rates p.674 ###
 ###Usage: ###
  adf.test(x, alternative = c("stationary", "explosive"),
