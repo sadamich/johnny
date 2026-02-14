6303d45e1e4141f34a50d20fd2dc7c2b0ecefb79
sadamich <sadamichikato@gmail.com>
2026-02-14 14:53:44 +0900

    Series

--- matrix_series.R
+++ matrix_series.R
@@ -18,3 +18,14 @@ sum(rep(1,8))
 sum(rep(2,8))
 sum(rep(5,8))
 sum(rep(10,8))
+
+w_testmt<- matrix(c(2,3,6,3,9,10,2,0,3,8,2,9,10,8,7),nrow=5)
+w_colsum<- numeric(nrow(w_testmt))
+for (i in 1:5){
+w_colsum[i]<- sum(w_testmt[i,]*c(1,0.5,0.3))
+}
+w_colsum
+
+W<- w_testmt
+v<- c(1,0.5,0.3)
+W%*%v
