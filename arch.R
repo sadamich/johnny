4301f1f17aeab9b2be549f06d604166e68c3c9ad
sadamich <sadamichikato@gmail.com>
2026-02-19 14:12:37 +0900

    ARCH

--- arch.R
+++ arch.R
@@ -6,9 +6,27 @@ library(forecast)
 xm702<- read.csv("xm702.csv", header =TRUE)
 str(xm702)
 attach(xm702)
-acf(DJRET)
+### Autocorrelation functions, exhibit 7 25 a (p.632(                      ###
+Acf(DJRET,lag.max = NULL,plot = TRUE,na.action = na.contiguous,
+  demean = TRUE)
+Pacf(DJRET,lag.max = NULL,plot = TRUE, na.action = na.contiguous,
+  demean = TRUE)
+Acf(DJRET^2,lag.max = NULL,plot = TRUE,na.action = na.contiguous,
+  demean = TRUE)
+Pacf(DJRET^2,lag.max = NULL,plot = TRUE, na.action = na.contiguous,
+  demean = TRUE)
 ### Exhibit 7 25 b (p.632)                                                 ###
 hist(DJRET)
 summary(DJRET)
+library(tseries)
+x<- na.remove(DJRET)
+x.arch <- garch(x, order = c(0,5))  # Fit ARCH(5) 
+### Compare with Panel 3 (p.632)                                           ###
+summary(x.arch)                     # Diagnostic tests
+plot(x.arch)                        
+x.garch <- garch(x, order = c(2,2))  # Fit GARCH(2,2) 
+### Compare with Panel 6 (p.633)                                           ###
+summary(x.garch)                     # Diagnostic tests
+plot(x.garch)                        
 
 
