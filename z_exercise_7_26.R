98e01ac12bb179cdc1e9da23d29a036369846817
sadamich <sadamichikato@gmail.com>
2026-02-22 13:23:09 +0900

    Orange

---
+++ z_exercise_7_26.R
@@ -0,0 +1,6 @@
+### Business and Economics. Oxford University Press                        ###
+### https://global.oup.com/booksites/content/0199268010/                   ###
+### Exercise 7 26 (p.721)                                                  ###
+xr726 <- read.csv("xr726.csv", header =TRUE)
+str(xr726)
+attach(xr726)
