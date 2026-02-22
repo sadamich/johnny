9774398212bbcae9d53f767784ec3dcc07c5e80e
sadamich <sadamichikato@gmail.com>
2026-02-22 13:21:40 +0900

    Fashion

---
+++ z_exercise_7_25.R
@@ -0,0 +1,8 @@
+### Business and Economics. Oxford University Press                        ###
+### https://global.oup.com/booksites/content/0199268010/                   ###
+### Exercise 7 25 (p.720)                                                  ###
+xr725 <- read.csv("xr725.csv", header =TRUE)
+str(xr725)
+attach(xr725)
+
+
