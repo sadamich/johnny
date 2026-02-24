c6f2b9812916bd5f131a3f2bd0348a0f5d7387dd
sadamich <sadamichikato@gmail.com>
2026-02-24 14:53:33 +0900

    VECM

--- vecm_comparison.R
+++ vecm_comparison.R
@@ -19,6 +19,13 @@ panel05<- VECM(
   exogen = NULL
 )
 panel05
+                          ECT    AAA -1 US3MTBIL -1     AAA -2 US3MTBIL -2
+Equation AAA      -0.02307544 0.5142587 -0.04493965 -0.3313776  0.03443276
+Equation US3MTBIL  0.03794585 0.7739789  0.17161767 -0.5223374 -0.03099130
+
+coefA(panel05)
+coefB(panel05)
+coefPI(panel05)
 
 panel06<- VECM(
   data = var_data,
@@ -34,10 +41,18 @@ panel06
                           ECT    AAA -1 US3MTBIL -1     AAA -2 US3MTBIL -2
 Equation AAA      -0.01873009 0.5137194  -0.0436500 -0.3332185  0.03641944
 Equation US3MTBIL  0.03391977 0.7776206   0.1699271 -0.5166337 -0.03244040
+coefA(panel06)
+coefB(panel06)
+coefPI(panel06)
+
+
+
 library(urca)
 panel05_eigen<- ca.jo(var_data, type = "eigen", ecdet = "trend", K = 2,
 spec= "longrun", season = NULL, dumvar = NULL)
 summary(panel05_eigen)
+
+cajolst(var_data, trend = TRUE, K = 2, season = NULL)
 
 panel06_eigen<- ca.jo(var_data, type = "eigen", ecdet = "none", K = 2,
 spec= "longrun", season = NULL, dumvar = NULL)
