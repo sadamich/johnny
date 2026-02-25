f974e05ec5480eb6891763f304588294461bd628
sadamich <sadamichikato@gmail.com>
2026-02-25 17:57:38 +0900

    Educ

---
+++ mlogit_ecducation.R
@@ -0,0 +1,29 @@
+### Source:Christiaan Heij, Paul de Boer, Philip Hans Franses, Teun Kloek, ###
+### Herman K. van Dijk (2004).Econometric Methods with Applications in     ###
+### Business and Economics. Oxford University Press                        ###
+### https://global.oup.com/booksites/content/0199268010/                   ###
+### Example 6 4 Bank wages ###
+xm604<- read.csv("xm604.csv", header = TRUE)
+attach(xm604)
+str(xm604)
+sxm604<- subset(xm604, GENDER==1)
+str(sxm604)
+attach(sxm604)
+
+data<- dfidx(sxm604, subset = c("DUMJCAT1","DUMJCAT2","DUMJCAT3"))
+library("Formula")
+f<- formula(DUMJCAT1 ~ EDUC +MINORITY)
+
+eq1<- mlogit(f,data)
+summary(eq1)
+
+
+MC <- dfidx(ModeCanada, subset = noalt == 4,
+            alt.levels = c("train", "air", "bus", "car"))
+
+f <- Formula(choice ~ cost | income + urban | ivt)
+f2 <- Formula(choice ~ cost + ivt | income + urban)
+f2 <- Formula(choice ~ cost + ivt | income + urban | 0)
+f3 <- Formula(choice ~ 0 | income | 0)
+eq_ex<- mlogit(f, MC)
+summary(eq_ex)
