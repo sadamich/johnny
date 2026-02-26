4c74920f3a03ead1e8d1ef3b5fdbbd92b4eb0012
sadamich <sadamichikato@gmail.com>
2026-02-26 15:21:26 +0900

    Survival

--- survival.R
+++ survival.R
@@ -40,3 +40,53 @@ install.packages("survival")
 library(survival)
 eq_suv<- coxph(Surv(log(STRIKECENS80)) ~ PROD, xm609)
 summary(eq_suv)
+eq_ex<- aareg(Surv(log(STRIKECENS80)) ~ PROD, xm609)
+summary(eq_ex)
+
+lfit <- aareg(Surv(time, status) ~ age + sex + ph.ecog, data=lung,
+                     nmin=1)
+
+
+survreg(, data, weights, subset, 
+        na.action, dist="weibull", init=NULL, scale=0, 
+        control,parms=NULL,model=FALSE, x=FALSE,
+        y=TRUE, robust=FALSE, cluster, score=FALSE, ...)
+
+install.packages("censReg")
+library("censReg")
+cf) Arne Henningsen
+### Panel 12 Tobit Model (p.519)                                           ###
+panel_12<- censReg(log(STRIKECENS80)~ 1)
+summary(panel_12)
+Call: censReg(formula = log(STRIKECENS80) ~ 1)
+Observations:
+         Total  Left-censored     Uncensored Right-censored 
+            62              1             61              0 
+Coefficients:
+            Estimate Std. error t value Pr(> t)    
+(Intercept)  3.01981    0.15256  19.794  <2e-16 ***
+logSigma     0.18264    0.09105   2.006  0.0449 *  
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+Newton-Raphson maximisation, 4 iterations
+Return code 1: gradient close to zero (gradtol)
+Log-likelihood: -99.25352 on 2 Df
+### Panel 13 Tobit Model (p.519)                                           ###
+panel_13<- censReg(log(STRIKECENS80)~ PROD)
+summary(panel_13)
+Call: censReg(formula = log(STRIKECENS80) ~ PROD)
+Observations:
+         Total  Left-censored     Uncensored Right-censored 
+            62              1             61              0 
+Coefficients:
+            Estimate Std. error t value Pr(> t)    
+(Intercept)  3.10910    0.14912  20.850  <2e-16 ***
+PROD        -8.11208    3.15550  -2.571  0.0101 *  
+logSigma     0.13220    0.09102   1.452  0.1464    
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+Newton-Raphson maximisation, 3 iterations
+Return code 8: successive function values within relative tolerance limit (reltol)
+Log-likelihood: -96.10716 on 3 Df
+
+
+
+
