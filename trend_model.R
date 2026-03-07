be5dc0673401d3395127d3929976eca7e68372fe
sadamich <sadamichikato@gmail.com>
2026-03-07 17:09:32 +0900

    Trend model

--- trend_model.R
+++ trend_model.R
@@ -132,6 +132,7 @@ y_rw<- rw_model(Y, lag=1, drift = TRUE)
 ### Compare with exhibit 7 14 e (p.591)                                    ###
 forecast(y_rw, h = 10) |> autoplot()
 
+dy<- Y[45:180] - Y[44:179]
 str(dy)
 y_1<- Y[44:179]
 str(y_1)
@@ -172,18 +173,61 @@ Model 2: dy ~ 1
 2    135 0.058997 -2 -0.0044763 5.4599 0.005262 **
 Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 
 
-str(Y)
-Dy<- Y[2:195]-Y[1:194]
-str(Dy)
-Ddy<- Dy[45:180] - Ddy[44:179]
-str(Ddy)
-Ddy1<- Ddy[44:179] - Ddy[43:178]
-Ddy2<- Ddy[43:178] - Ddy[42:177]
-Ddy3<- Ddy[42:177] - Ddy[41:176]
-Ddy4<- Ddy[41:176] - Ddy[40:175]
-Ddy5<- Ddy[40:175] - Ddy[39:174]
-eq_d_adf<- lm(Ddy ~ y_1 + Ddy1+Ddy2+Ddy3+Ddy4+Ddy5 + t)
+dy<- Y[45:180] - Y[44:179]
+dy1<- Y[44:179] - Y[43:178]
+dy2<- Y[43:178] - Y[42:177]
+dy3<- Y[42:177] - Y[41:176]
+dy4<- Y[41:176] - Y[40:175]
+dy5<- Y[40:175] - Y[39:174]
+eq_d_adf<- lm(dy ~ y_1 + dy1+dy2+dy3+dy4+dy5 + t)
 summary(eq_d_adf)
+### Panel 5(p. 601)Call: lm(formula = dy ~ y_1 + dy1 + dy2 + dy3           ###
+###                                        + dy4 + dy5 + t)                ###
+Residuals:
+      Min        1Q    Median        3Q       Max 
+-0.064883 -0.007754  0.001295  0.009187  0.053379 
+Coefficients:
+              Estimate Std. Error t value Pr(>|t|)    
+(Intercept)  0.2468602  0.0818114   3.017 0.003076 ** 
+y_1         -0.0629964  0.0218799  -2.879 0.004675 ** 
+dy1          0.2672903  0.0803538   3.326 0.001148 ** 
+dy2          0.1118044  0.0778542   1.436 0.153421    
+dy3         -0.1501269  0.0768446  -1.954 0.052925 .  
+dy4          0.3456369  0.0763037   4.530 1.34e-05 ***
+dy5         -0.2897889  0.0791537  -3.661 0.000366 ***
+t            0.0003974  0.0001657   2.398 0.017922 *  
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+Residual standard error: 0.01777 on 128 degrees of freedom
+Multiple R-squared:  0.3148,    Adjusted R-squared:  0.2773 
+F-statistic: 8.402 on 7 and 128 DF,  p-value: 1.979e-08
+
+anova(eq_d_adf)
+Analysis of Variance Table
+Response: dy
+           Df   Sum Sq   Mean Sq F value    Pr(>F)    
+y_1         1 0.002376 0.0023762  7.5242 0.0069612 ** 
+dy1         1 0.000626 0.0006258  1.9815 0.1616580    
+dy2         1 0.002115 0.0021149  6.6967 0.0107750 *  
+dy3         1 0.001973 0.0019727  6.2464 0.0137104 *  
+dy4         1 0.003922 0.0039223 12.4199 0.0005898 ***
+dy5         1 0.005745 0.0057453 18.1922 3.849e-05 ***
+t           1 0.001816 0.0018162  5.7511 0.0179218 *  
+Residuals 128 0.040424 0.0003158                      
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1
+eq_d_adf2<- lm(dy ~ dy1+dy2+dy3+dy4+dy5)
+anova(eq_d_adf, eq_d_adf2)
+Analysis of Variance Table
+Model 1: dy ~ y_1 + dy1 + dy2 + dy3 + dy4 + dy5 + t
+Model 2: dy ~ dy1 + dy2 + dy3 + dy4 + dy5
+  Res.Df      RSS Df  Sum of Sq      F   Pr(>F)   
+1    128 0.040424                                 
+2    130 0.043893 -2 -0.0034695 5.4931 0.005144 **
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 
+
+### Panel 7 (p.601)                                                       ###
+res<- resid(eq_d_adf)
+acf(res)
+
 ### Seasonality                                                            ###
 ### ADF Test for checking the seasonality: H0 is I(2)                      ###
 ### ADF Test, compare with panel 1,panel 3 and panel 4 (p.609)             ###                                                                 ###
@@ -203,7 +247,27 @@ adfTest(YEARSUMY[45:180], lags = 1, type = "ct", title = NULL,
 adfTest(YEARSUMY[45:180], lags = 4, type = "ct", title = NULL)
 d_YEAR <- YEARSUMY[45:180] - YEARSUMY[44:179]
 adfTest(d_YEAR, lags = 4, type = "c", title = NULL)
-
+### Panel 1 (p.609)
+dy<- Y[45:180] - Y[44:179]
+dy_1<- Y[44:179] - Y[43:178]
+Ddy<- dy[2:136] - dy[1:135]
+Ddy1<- c(NA,Ddy)[1:135]
+Ddy2<- c(NA,Ddy1)[1:135]
+Ddy3<- c(NA,Ddy2)[1:135]
+Ddy4<- c(NA,Ddy3)[1:135]
+eq_adf_s<- lm(Ddy ~ dy_1[1:135] + Ddy1 + Ddy2 + Ddy3 + Ddy4)
+summary(eq_adf_s)
+acf(dy)
+str(YEARSUMY)
+d_YEAR <- YEARSUMY[45:180] - YEARSUMY[44:179]
+y_year_1<- YEARSUMY[44:179]
+d_YEAR1<- c(NA,d_YEAR)[1:136]
+d_YEAR2<- c(NA,d_YEAR1)[1:136]
+d_YEAR3<- c(NA,d_YEAR2)[1:136]
+d_YEAR4<- c(NA,d_YEAR3)[1:136]
+t<- 1:136
+eq_s<- lm(d_YEAR ~ y_year_1 + d_YEAR1 + d_YEAR2 + d_YEAR3 + d_YEAR4 + t)
+summary(eq_s)
 ### Seasonal plot                                                          ###
 ggseasonplot(y, col = rainbow(12), year.labels = TRUE)
 ggseasonplot(y, year.labels = TRUE, continuous = TRUE)
