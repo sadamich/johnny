7d86feb5cce207cb49ad9cee39bdc041ef4d1bdb
sadamich <sadamichikato@gmail.com>
2026-03-09 16:34:09 +0900

    CAPM

--- finance_capm.R
+++ finance_capm.R
@@ -19,5 +19,33 @@ Coefficients:
 RENDMARK      1.1711e+00   6.9309e-02   1.6897e+01   4.7226e-64
 J-Test: degrees of freedom is 0 
                 J-test                P-value             
-Test E(g)=0:    8.07282570225798e-26  *******             
+Test E(g)=0:    8.07282570225798e-26  *******
+
+### Compare with the Panel 3 (p.264)                                       ###
+const<- rep(1, 240)
+set.seed(74)
+eps<- rt(240, 5)
+X<- cbind(const, RENDMARK, eps)
+y<- RENDCYCO
+negSSE <- function(beta) {
+e <- y- X %*% beta
+-crossprod(e)
+}
+m <- maxLik(negSSE, start=c(0,0,1))
+summary(m, eigentol=1e-15)
+
+Maximum Likelihood estimation
+Newton-Raphson maximisation, 3 iterations
+Return code 8: successive function values within relative tolerance limit (reltol)
+Log-Likelihood: -7310.359 
+3  free parameters
+Estimates:
+     Estimate Std. error t value Pr(> t)    
+[1,] -0.44857    0.04634  -9.681  <2e-16 ***
+[2,]  1.17137    0.00962 121.766  <2e-16 ***
+[3,] -0.06745    0.03884  -1.736  0.0825 .  
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+
+             
  
