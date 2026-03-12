4294c80d9784ebe6038b72439f1aa28657fe535c
sadamich <sadamichikato@gmail.com>
2026-03-12 14:11:44 +0900

    Box Cox Transformation

--- ml_box_cox.R
+++ ml_box_cox.R
@@ -14,18 +14,20 @@ loglik <- function(theta) {
    beta4<- theta[4]                        
    lambda<- theta[5]
    sigma <- theta[6]
-   y_lam<- ((SALARY/10000)^lambda - 1)/lambda
+   S<- SALARY/10000
+   y_lam<- (S^lambda - 1)/lambda
    EDUC_lam <- (EDUC^lambda -1)/lambda
    GENDER_lam <- (GENDER^lambda -1)/lambda
    MINORITY_lam <- (MINORITY^lambda -1)/lambda
    mu<- (beta^lambda -1)/lambda + beta2*GENDER_lam + beta3*MINORITY_lam + 
          beta4*EDUC_lam
    ll <- -0.5*N*log(2*pi) - 0.5*N*log(sigma^2) 
-          -(lambda -1)*sum(log(SALARY/10000))- sum(0.5*(y_lam - mu)^2/sigma^2)                           
-   
+          -(lambda -1)*sum(log(S))- sum(0.5*(y_lam - mu)^2/sigma^2)                           
 }
 N <- 474
 library(maxLik)
 m<- maxLik(loglik, start=c(0,0,0,0,0,0))
 summary(m)
 ???
+ -0.5*N*log(2*pi)- N*log(sigma) + (lambda -1)*sum(log(S)) 
+- sum(0.5*(y- mu)^2/sigma^2)
