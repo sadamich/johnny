bd16d83cde2cd15c8e65c87538ece514f368ea32
sadamich <sadamichikato@gmail.com>
2026-03-14 14:33:55 +0900

    IV estimation

---
+++ iv.R
@@ -0,0 +1,57 @@
+### Source:Christiaan Heij, Paul de Boer, Philip Hans Franses, Teun Kloek, ###
+### Herman K. van Dijk (2004).Econometric Methods with Applications in     ###
+### Business and Economics. Oxford University Press                        ###
+### https://global.oup.com/booksites/content/0199268010/                   ###
+xm531<- read.csv("xm531.csv", header = TRUE)
+str(xm531)
+attach(xm531)
+panel01<- lm(GC ~ PG+RI)
+summary(panel01)
+### Panel 1 Call:lm(formula = GC ~ PG + RI)
+Residuals:
+      Min        1Q    Median        3Q       Max 
+-0.046139 -0.013715 -0.000357  0.020523  0.038915 
+Coefficients:
+            Estimate Std. Error t value Pr(>|t|)    
+(Intercept)  4.98600    0.08110   61.48   <2e-16 ***
+PG          -0.52758    0.02632  -20.05   <2e-16 ***
+RI           0.57322    0.02451   23.39   <2e-16 ***
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+Residual standard error: 0.02385 on 27 degrees of freedom
+Multiple R-squared:  0.9872,    Adjusted R-squared:  0.9862 
+F-statistic:  1037 on 2 and 27 DF,  p-value: < 2.2e-16
+
+panel03<- lm(PG ~ RPT+RPN+RPU+RI)
+summary(panel03)
+### Panel 3 (p.404) Call:lm(formula = PG ~ RPT + RPN + RPU + RI)           ###
+Residuals:
+      Min        1Q    Median        3Q       Max 
+-0.160161 -0.036024 -0.002891  0.034824  0.175059 
+Coefficients:
+            Estimate Std. Error t value Pr(>|t|)    
+(Intercept)   7.7410     0.8337   9.285 1.40e-09 ***
+RPT          -0.8080     0.1912  -4.225 0.000277 ***
+RPN          -3.5279     0.3520 -10.023 3.06e-10 ***
+RPU           0.2331     0.1831   1.273 0.214765    
+RI           -2.2984     0.2471  -9.303 1.35e-09 ***
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+Residual standard error: 0.07269 on 25 degrees of freedom
+Multiple R-squared:  0.8868,    Adjusted R-squared:  0.8687 
+F-statistic: 48.97 on 4 and 25 DF,  p-value: 1.797e-11
+
+pg_fit<- fitted(panel03)
+panel02<- lm(GC ~ pg_fit + RI)
+summary(panel02)
+### Panel 2 (p.404) Call:lm(formula = GC ~ pg_fit + RI)                    ###
+Residuals:
+      Min        1Q    Median        3Q       Max 
+-0.054154 -0.025003 -0.008033  0.012243  0.090373 
+Coefficients:
+            Estimate Std. Error t value Pr(>|t|)    
+(Intercept)  5.01370    0.13386   37.46  < 2e-16 ***
+pg_fit      -0.54445    0.04618  -11.79 3.72e-12 ***
+RI           0.56466    0.04050   13.94 7.46e-14 ***
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+Residual standard error: 0.03833 on 27 degrees of freedom
+Multiple R-squared:  0.9668,    Adjusted R-squared:  0.9644 
+F-statistic: 393.3 on 2 and 27 DF,  p-value: < 2.2e-16
