26d037d8d1110284fa1bf12cbcf4818180363177
sadamich <sadamichikato@gmail.com>
2026-03-18 17:03:41 +0900

    Chow break test

--- structure_chow_test.R
+++ structure_chow_test.R
@@ -52,4 +52,39 @@ Warning message:
 In anova.lmlist(object, ...) :
   models with response ‘c("LOGSAL_12", "LOGSAL_3")’                     
 
-
+### Exsample 5 9 Bank wages (p.317)                                        ###
+xm501<- read.csv("xm501.csv",header =TRUE)
+str(xm501)
+attach(xm501)
+panel01<- lm(LOGSALARY ~ GENDER+MINORITY+EDUC)
+LOGSALARY_2<- LOGSALARY[EDUC<=16]
+GENDER_2<- GENDER[EDUC<=16]
+MINORITY_2<- MINORITY[EDUC<=16]
+EDUC_2<- EDUC[EDUC<=16]
+panel02<- lm(LOGSALARY_2~GENDER_2+MINORITY_2+EDUC_2)
+LOGSALARY_3<- LOGSALARY[EDUC>=17]
+GENDER_3<- GENDER[EDUC>=17]
+MINORITY_3<- MINORITY[EDUC>=17]
+EDUC_3<- EDUC[EDUC>=17]
+panel03<- lm(LOGSALARY_3~GENDER_3+MINORITY_3+EDUC_3)
+anova(panel01,panel02,panel03)
+res_2<- resid(panel02)
+ssr_2<- sum(res_2^2)
+[1] 23.40327
+res_3<- resid(panel03)
+ssr_3<- sum(res_3^2)
+[1] 2.941173
+### 5 20 (p.315)                                                           ###
+F_break<- function(ssr, ssr2,ssr3,k,n2,n3){
+result<- ((ssr - ssr2 -ssr3)/k) / ((ssr2+ssr3)/(n2+n3 - 2*k))
+return(result)
+}
+F_break(30.85177,23.40327,2.941173,4,424,50)
+[1] 19.93223
+### 5 22 (p.316)                                                           ###
+F_forecast<- function(ssr, ssr2,k,n2,n3){
+result<- ((ssr - ssr2)/n3) / (ssr2/(n2-k))
+return(result)
+}
+F_forecast(30.85177, 23.40327,4,424,50)
+[1] 2.673447
