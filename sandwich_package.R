8343009cda899ea24a85154fecbebf9e1448ae99
sadamich <sadamichikato@gmail.com>
2026-03-19 13:29:37 +0900

    Sandwich GMM

---
+++ sandwich_package.R
@@ -0,0 +1,27 @@
+### https://cran.r-project.org/web/packages/sandwich/vignettes/sandwich-OOP.pdf ###
+### 4 1 Bread 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+### https://cran.r-project.org/web/packages/sandwich/vignettes/sandwich-CL.pdf ###
+### 3 Methods 3 1 Sandwich covariances
+Sandwich
+Bread    Hessian
+Meat     Gradient
+
+### 3 2 Clustered variancies
+
+
+
