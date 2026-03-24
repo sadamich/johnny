fd32caa15458a134dc9c85cccd5a9cfebec2d8bd
sadamich <sadamichikato@gmail.com>
2026-03-25 07:01:48 +0900

    Exe

---
+++ z_exercise_5_29.R
@@ -0,0 +1,7 @@
+### Source:Christiaan Heij, Paul de Boer, Philip Hans Franses, Teun Kloek, ###
+### Herman K. van Dijk (2004).Econometric Methods with Applications in     ###
+### Business and Economics. Oxford University Press                        ###
+### https://global.oup.com/booksites/content/0199268010/                   ###
+xm526<- read.csv("xm526.csv", header=TRUE)
+str(xm526)
+attach(xm526)
