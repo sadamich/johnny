29946117f78f9db999d737c87fb44ea658edac40
sadamich <sadamichikato@gmail.com>
2026-03-27 14:16:08 +0900

    Exe

---
+++ z_exercise_4_8.R
@@ -0,0 +1,19 @@
+### Source:Christiaan Heij, Paul de Boer, Philip Hans Franses, Teun Kloek, ###
+### Herman K. van Dijk (2004).Econometric Methods with Applications in     ###
+### Business and Economics. Oxford University Press                        ###
+### https://global.oup.com/booksites/content/0199268010/                   ###
+Exercise 4 8
+
+Multicolinearity
+
+Ommited bias
+
+Simulation
+
+y<- c + b2*x2 + b3*x3
+
+correlation(x2, x3)> 0
+
+
+
+
