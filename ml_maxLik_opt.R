37533bf4b7766dbbdd5be979691fc3b51fe6ea23
sadamich <sadamichikato@gmail.com>
2026-03-29 05:38:09 +0900

    Optimization

--- ml_maxLik_opt.R
+++ ml_maxLik_opt.R
@@ -1,48 +1,102 @@
-### Package maxLik ###
+library(maxLik)
 ### Source: https://cran.r-project.org/web/packages/maxLik/refman/maxLik.html   ###
 ### Compare with 4.2.3 Non-linear optimization of Christiaan Heij, Paul de      ###
 ### Boer, Philip Hans Franses,Teun Kloek,Herman K. van Dijk (2004).Econometric  ###
 ### Methods with Applications in Business and Economics. Oxford University Press###                           
 ### https://global.oup.com/booksites/content/0199268010/                        ###
-## Create a 'maxControl' object:
+### Create a 'maxControl' object                                           ###
 maxControl(tol=1e-4, sann_tmax=7, printLevel=2)
-## Optimize quadratic form t(D) %*% W %*% D with p.d. weight matrix,
-## s.t. constraints sum(D) = 1
+### Optimize quadratic form t(D) %*% W %*% D with p.d. weight matrix,
+### s.t. constraints sum(D) = 1
 quadForm <- function(D) {
    return(-t(D) %*% W %*% D)
 }
 eps <- 0.1
 W <- diag(3) + matrix(runif(9), 3, 3)*eps
 D <- rep(1/3, 3)
+
+### diag(3)
+###     [,1] [,2] [,3]
+###[1,]    1    0    0
+###[2,]    0    1    0
+###[3,]    0    0    1
+### W
+###           [,1]       [,2]       [,3]
+###[1,] 1.07572376 0.05246460 0.08457623
+###[2,] 0.09922296 1.03721399 0.07287978
+###[3,] 0.09140478 0.06616082 1.00344095
+### D
+###[1] 0.3333333 0.3333333 0.3333333
                 # initial values
-## create control object and use it for optimization
+### create control object and use it for optimization                     ###
 co <- maxControl(printLevel=2, qac="marquardt", marquardt_lambda0=1)
 res <- maxNR(quadForm, start=D, control=co)
 ### Marquart Hessian correction ###
 print(summary(res))
+Newton-Raphson maximisation with Marquardt (1963) Hessian correction 
+Number of iterations: 5 
+Return code: 1 
+gradient close to zero (gradtol) 
+Function value: -3.065299e-14 
+Estimates:
+         estimate      gradient
+[1,] 6.134875e-08 -1.668574e-07
+[2,] 9.687775e-08 -2.262107e-07
+[3,] 1.146353e-07 -2.543257e-07
 
-## Now perform the same with no trace information
+### Now perform the same with no trace information                         ###
 co <- maxControl(co, printLevel=0)
 res <- maxNR(quadForm, start=D, control=co) # no tracing information
 print(summary(res))  # should be the same as above
+Newton-Raphson maximisation with Marquardt (1963) Hessian correction 
+Number of iterations: 5 
+Return code: 1 
+gradient close to zero (gradtol) 
+Function value: -3.065299e-14 
+Estimates:
+         estimate      gradient
+[1,] 6.134875e-08 -1.668574e-07
+[2,] 9.687775e-08 -2.262107e-07
+[3,] 1.146353e-07 -2.543257e-07
 maxControl(res) # shows the control structure
 
-
-## a two-dimensional exponential hat
+### a two-dimensional exponential hat                                      ###
 f <- function(a) exp(-a[1]^2 - a[2]^2)
-## maximize wrt. both parameters 
+### maximize wrt. both parameters                                          ###
 free <- maxNR(f, start=1:2) 
 summary(free)  # results should be close to (0,0)
+Newton-Raphson maximisation 
+Number of iterations: 6 
+Return code: 1 
+gradient close to zero (gradtol) 
+Function value: 1 
+Estimates:
+          estimate      gradient
+[1,] -3.234570e-08  6.472600e-08
+[2,]  3.943876e-07 -7.888135e-07
 activePar(free)
+
 ## keep the first parameter constant
 cons <- maxNR(f, start=1:2, fixed=c(TRUE,FALSE))
 summary(cons) # result should be around (1,0)
 activePar(cons)
 
-## A simple example with sin(x)' = cos(x)
+### A simple example with sin(x)' = cos(x)                                 ###
 f <- function(x) c(sin=sin(x))
 Dsin <- compareDerivatives(f, cos, t0=c(angle=1))
-### Example of normal log-likelihood.  Two-parameter function.###
+-------- compare derivatives -------- 
+Note: analytic gradient is vector.  Transforming into a matrix form
+Function value:
+[1] 0.841471
+Dim of analytic gradient: 1 1 
+       numeric          : 1 1 
+      
+param  theta 0  analytic   numeric      rel.diff
+  [1,]       1 0.5403023 0.5403023 -5.129895e-11
+Max relative difference: 5.129895e-11 
+-------- END of compare derivatives -------- 
+
+### Example of normal log-likelihood.  Two-parameter function.             ###
 x <- rnorm(100, 1, 2) # generate rnorm x
 l <- function(b) sum(dnorm(x, mean=b[1], sd=b[2], log=TRUE))
 gradl <- function(b) {
@@ -50,13 +104,44 @@ gradl <- function(b) {
     sigma=sum((x - b[1])^2/b[2]^3 - 1/b[2]))
 }
 gradl. <- compareDerivatives(l, gradl, t0=c(mu=1,sigma=2))
-## An example with f returning a vector, t0 = a scalar
+-------- compare derivatives -------- 
+Note: analytic gradient is vector.  Transforming into a matrix form
+Function value:
+[1] -208.3779
+Dim of analytic gradient: 1 2 
+       numeric          : 1 2 
+t0
+     mu sigma
+[1,]  1     2
+analytic gradient
+           [,1]     [,2]
+[1,] -0.6438967 -2.83065
+numeric gradient
+             mu    sigma
+[1,] -0.6438967 -2.83065
+(anal-num)/(0.5*(abs(anal)+abs(num)))
+              mu         sigma
+[1,] 6.14865e-10 -1.182815e-09
+Max relative difference: 1.182815e-09 
+-------- END of compare derivatives -------- 
+### An example with f returning a vector, t0 = a scalar                    ###
 trig <- function(x)c(sin=sin(x), cos=cos(x))
 Dtrig <- function(x)c(sin=cos(x), cos=-sin(x))
 Dtrig. <- compareDerivatives(trig, Dtrig, t0=1)
-
+-------- compare derivatives -------- 
+Note: analytic gradient is vector.  Transforming into a matrix form
+Function value:
+[1] 0.8414710 0.5403023
+Dim of analytic gradient: 2 1 
+       numeric          : 2 1 
+      
+param  theta 0   analytic    numeric      rel.diff
+  [1,]       1  0.5403023  0.5403023 -5.129895e-11
+  [2,]       1 -0.8414710 -0.8414710  9.923691e-11
+Max relative difference: 9.923691e-11 
+-------- END of compare derivatives -------- 
 set.seed(0)
-### generate a simple nearly multicollinear dataset  ###
+### generate a simple nearly multicollinear dataset                        ###
 x1 <- runif(100)
 x2 <- runif(100)
 x3 <- x1 + x2 + 0.000001*runif(100) # this is virtually equal to x1 + x2
@@ -65,9 +150,39 @@ y <- x1 + x2 + x3 + x4 + rnorm(100)
 m <- lm(y ~ -1 + x1 + x2 + x3 + x4)
 print(summary(m)) # note the outlandish estimates and standard errors
                      # while R^2 is 0.88. This suggests multicollinearity
+Call:lm(formula = y ~ -1 + x1 + x2 + x3 + x4)
+Residuals:
+     Min       1Q   Median       3Q      Max 
+-2.01025 -0.50270  0.03936  0.49757  2.37986 
+Coefficients:
+     Estimate Std. Error t value Pr(>|t|)    
+x1  2.692e+05  2.969e+05   0.907  0.36692    
+x2  2.692e+05  2.969e+05   0.907  0.36692    
+x3 -2.692e+05  2.969e+05  -0.907  0.36692    
+x4  9.710e-01  2.728e-01   3.560  0.00058 ***
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+Residual standard error: 0.9427 on 96 degrees of freedom
+Multiple R-squared:  0.8891,    Adjusted R-squared:  0.8845 
+F-statistic: 192.5 on 4 and 96 DF,  p-value: < 2.2e-16
 condiNumber(model.matrix(m))   # note the value 'explodes' at x3
+### x1       1                                                           ###
+### x2       3.254384                                                    ###
+### x3       12176407                                                    ###
+### x4       10284611                                                    ### 
 ### we may test the results further:
 print(summary(lm(x3 ~ -1 + x1 + x2)))
+Call:lm(formula = x3 ~ -1 + x1 + x2)
+Residuals:
+       Min         1Q     Median         3Q        Max 
+-7.817e-07 -1.739e-07  4.917e-08  2.895e-07  6.657e-07 
+Coefficients:
+    Estimate Std. Error  t value Pr(>|t|)    
+x1 1.000e+00  9.744e-08 10263046   <2e-16 ***
+x2 1.000e+00  8.604e-08 11622140   <2e-16 ***
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+Residual standard error: 3.323e-07 on 98 degrees of freedom
+Multiple R-squared:      1,     Adjusted R-squared:      1 
+F-statistic: 4.753e+14 on 2 and 98 DF,  p-value: < 2.2e-16
 ### Note the extremely high t-values and R^2: x3 is (almost) completely ###
 ### explained by x1 and x2 (R^2 = 1)                                    ###
 
@@ -100,33 +215,33 @@ summary(max4)
 max4. <- maxLik(fn2max, start=c(1, 4), fixed=2)
 summary(max4.)
 
-### ML estimation of exponential duration model:                        ###
+### ML estimation of exponential duration model:                           ###
 t <- rexp(10, 2)
 loglik <- function(theta) log(theta) - theta*t
-### Estimate with numeric gradient and hessian                          ###
+### Estimate with numeric gradient and hessian                             ###
 a <- maxLik(loglik, start=1 )
 gradient(a)
-### Extract the gradients evaluated at each observation                 ###
+### Extract the gradients evaluated at each observation                    ###
 library( sandwich )
 estfun( a )
-### Estimate with analytic gradient.                                    ###
-### Note: it returns a vector                                           ###
+### Estimate with analytic gradient.                                       ###
+### Note: it returns a vector                                              ###
 gradlik <- function(theta) 1/theta - t
 b <- maxLik(loglik, gradlik, start=1)
 gradient(a)
 estfun( b )
 
-### log-likelihood for normal density                                 ###
-### a[1] - mean                                                       ###
-### a[2] - standard deviation                                         ###
+### log-likelihood for normal density                                      ###
+### a[1] - mean                                                            ###
+### a[2] - standard deviation                                              ###
 ll <- function(a) sum(-log(a[2]) - (x - a[1])^2/(2*a[2]^2))
 x <- rnorm(100) # sample from standard normal
 ml <- maxLik(ll, start=c(1,1))
-### ignore eventual warnings "NaNs produced in: log(x)"               ###
+### ignore eventual warnings "NaNs produced in: log(x)"                    ###
 summary(ml) # result should be close to c(0,1)
 hessian(ml) # How the Hessian looks like
 sqrt(-solve(hessian(ml))) #Note: standard deviations are on the diagonal#
-### Now run the same example while fixing a[2] = 1                    ###
+### Now run the same example while fixing a[2] = 1                         ###
 mlf <- maxLik(ll, start=c(1,1), activePar=c(TRUE, FALSE))
 summary(mlf) # first parameter close to 0, the second exactly 1.0
 hessian(mlf)
@@ -136,53 +251,53 @@ hessian(mlf)
 sqrt(-solve(hessian(mlf)[activePar(mlf), activePar(mlf)]))
 # gives the standard deviation for the mean
 
-### Maximum Likelihood estimation of Poissonian distribution          ###
+### Maximum Likelihood estimation of Poissonian distribution               ###
 n <- rpois(100, 3)
 loglik <- function(l) n*log(l) - l - lfactorial(n)
-### we use numeric gradient                                           ###
+### we use numeric gradient                                                ###
 summary(maxBFGS(loglik, start=1))
-### you would probably prefer mean(n) instead of that                 ###
-### Note also that maxLik is better suited for Maximum Likelihood     ###
-### Now an example of constrained optimization                        ###
+### you would probably prefer mean(n) instead of that                      ###
+### Note also that maxLik is better suited for Maximum Likelihood          ###
+### Now an example of constrained optimization                             ###
 f <- function(theta) {
   x <- theta[1]
   y <- theta[2]
   exp(-(x^2 + y^2))
   ## you may want to use exp(- theta %*% theta) instead
 }
-### use constraints: x + y >= 1                                       ###
+### use constraints: x + y >= 1                                            ###
 A <- matrix(c(1, 1), 1, 2)
 B <- -1
 res <- maxNM(f, start=c(1,1), constraints=list(ineqA=A, ineqB=B),
 control=list(printLevel=1))
 print(summary(res))
 
-### Fit exponential distribution by ML                                ###
+### Fit exponential distribution by ML                                     ###
 t <- rexp(100, 2)  # create data with parameter 2
 loglik <- function(theta) sum(log(theta) - theta*t)
-### Note the log-likelihood and gradient are summed over observations ###
+### Note the log-likelihood and gradient are summed over observations      ###
 gradlik <- function(theta) sum(1/theta - t)
 hesslik <- function(theta) -100/theta^2
-### Estimate with finite-difference gradient and Hessian              ###
+### Estimate with finite-difference gradient and Hessian                   ###
 a <- maxNR(loglik, start=1, control=list(printLevel=2))
 summary(a)
-### You would probably prefer 1/mean(t) instead                       ###
-### The same example with analytic gradient and Hessian               ###
+### You would probably prefer 1/mean(t) instead                            ###
+### The same example with analytic gradient and Hessian                    ###
 a <- maxNR(loglik, gradlik, hesslik, start=1)
 summary(a)
-### BFGS estimation with finite-difference gradient                   ###
+### BFGS estimation with finite-difference gradient                        ###
 a <- maxBFGSR( loglik, start=1 )
 summary(a)
-### For the BHHH method we need likelihood values and gradients       ###
-### of individual observations, not the sum of those                  ###
+### For the BHHH method we need likelihood values and gradients            ###
+### of individual observations, not the sum of those                       ###
 loglikInd <- function(theta) log(theta) - theta*t
 gradlikInd <- function(theta) 1/theta - t
-### Estimate with analytic gradient                                   ###
+### Estimate with analytic gradient                                        ###
 a <- maxBHHH(loglikInd, gradlikInd, start=1)
 summary(a)
-### Example with a vector argument:  Estimate the mean and            ###
-### variance of a random normal sample by maximum likelihood          ###
-### Note: you might want to use maxLik instead                        ###
+### Example with a vector argument:  Estimate the mean and                 ###
+### variance of a random normal sample by maximum likelihood               ###
+### Note: you might want to use maxLik instead                             ###
 loglik <- function(param) {
                            # param is a 2-vector of c(mean, sd)
   mu <- param[1]
@@ -194,18 +309,18 @@ x <- rnorm(100, 1, 2) # use mean=1, sd=2
 N <- length(x)
 res <- maxNR(loglik, start=c(0,1)) # use 'wrong' start values
 summary(res)
-### The previous example with named parameters and a fixed value      ###
+### The previous example with named parameters and a fixed value           ###
 resFix <- maxNR(loglik, start=c(mu=0, sigma=1), fixed="sigma")
 summary(resFix)  # 'sigma' is exactly 1.000 now.
-### Constrained optimization                                          ###
-### We maximize exp(-x^2 - y^2) where x+y = 1                         ###
+### Constrained optimization                                               ###
+### We maximize exp(-x^2 - y^2) where x+y = 1                              ###
 hatf <- function(theta) {
   x <- theta[1]
   y <- theta[2]
   exp(-(x^2 + y^2))
   ## Note: you may prefer exp(- theta %*% theta) instead
 }
-### use constraints: x + y = 1                                        ###
+### use constraints: x + y = 1                                             ###
 A <- matrix(c(1, 1), 1, 2)
 B <- -1
 res <- maxNR(hatf, start=c(0,0), constraints=list(eqA=A, eqB=B),
@@ -217,9 +332,7 @@ library(maxLik)
 X <-cbind(rnorm(100), rnorm(100, sd=1e3), rnorm(100, sd=1e7))
 y <-X%*%c(1,1,1)+ rnorm(100)
 negSSE <- function(beta) {
-
 e <- y- X %*% beta
-
 -crossprod(e)
  }
 m <- maxLik(negSSE, start=c(0,0,0))
