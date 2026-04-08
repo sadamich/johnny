c819b51be43efea1db7c9dc5b70a755017c7cd5c
sadamich <sadamichikato@gmail.com>
2026-04-08 13:26:09 +0900

    Interest and Hetero

--- z_exercise_5_28.R
+++ z_exercise_5_28.R
@@ -5,3 +5,13 @@
 xr528<- read.csv("xr528.csv", header =TRUE)
 str(xr528)
 attach(xr528)
+detach(xr528)
+
+x<- US3MTBIL[2:180] - US3MTBIL[1:179]
+str(x)
+eq<- lm(x~ US3MTBIL[1:179])
+summary(eq)
+res<- resid(eq)
+eq_var<- lm(res^2 ~ x)
+summary(eq_var)
+179* 0.06691 = 11.97689
