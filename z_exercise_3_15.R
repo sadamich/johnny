0fea1c090010a2d57b594261045bdc286d15716d
sadamich <sadamichikato@gmail.com>
2026-05-07 13:07:52 +0900

    F Test and R_sq

--- z_exercise_3_15.R
+++ z_exercise_3_15.R
@@ -7,22 +7,33 @@ str(xr315)
 attach(xr315)
 eq_base<- lm(LOGY ~ LOGL + LOGK)
 summary(eq_base)
-### Base Model Regression Call:lm(formula = LOGY ~ LOGL + LOGK)            ###
+### Proble a Base Model Regression Call:lm(formula = LOGY ~ LOGL + LOGK)   ###
 Residuals:
      Min       1Q   Median       3Q      Max 
 -0.65794 -0.16933 -0.04051  0.16689  0.62780 
 Coefficients:
             Estimate Std. Error t value Pr(>|t|)    
 (Intercept)  0.70083    0.41524   1.688   0.1050    
-LOGL         0.75632    0.09145   8.270 2.42e-08 ***
-LOGK         0.24182    0.11017   2.195   0.0385 *  
+LOGL         0.75632    0.09145   8.270 2.42e-08 ***   significant
+LOGK         0.24182    0.11017   2.195   0.0385 *     significant
 Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
 Residual standard error: 0.2817 on 23 degrees of freedom
 Multiple R-squared:  0.9569,    Adjusted R-squared:  0.9531 
 F-statistic: 255.2 on 2 and 23 DF,p-value:2.2e-16 (H0 b2 = b3 = 0 is rejectet)
 res<- resid(eq_base)
 e<- sum(res^2)
-### The same 
+library(car)
+linearHypothesis(eq_base, "1*LOGL + 1*LOGK = 0")
+Linear hypothesis test:
+LOGL  + LOGK = 0
+Model 1: restricted model
+Model 2: LOGY ~ LOGL + LOGK
+  Res.Df    RSS Df Sum of Sq      F    Pr(>F)    
+1     24 32.789                                  
+2     23  1.826  1    30.964 390.11 6.352e-16 ***
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+### The same (equal) coefficients Model                                    ### 
 log_lk<- LOGL+LOGK
 eq_same<- lm(LOGY ~ log_lk)
 summary(eq_same)
@@ -45,7 +56,8 @@ log_lk_c<- LOGL - LOGK
 eq_c<- lm(LOGY - LOGK ~ log_lk_c)
 summary(eq_c)
 
-### The constant return to scal model :lm(formula = LOGY - LOGK ~ log_lk_c)###
+### The constant return to scal model                                      ###
+### lm(formula = LOGY - LOGK ~ log_lk_c)                                   ###
 Residuals:
     Min      1Q  Median      3Q     Max 
 -0.6564 -0.1679 -0.0411  0.1665  0.6268 
@@ -90,7 +102,6 @@ return(result)
 F(26,2, 0.944)
 F-statistic: 404.4 on 1 and 24 DF,  p-value: < 2.2e-16F<- function(n, k, R){
 
-
 anova(eq_base, eq_c) 
 Analysis of Variance Table
 Response: LOGY
@@ -117,7 +128,6 @@ return(result)
 }
 F(1.825652,1.825544,26,3,2)
 [1] 0.0006803451
-
 
 F<- function(n, k, R){
 result<- (n-k)/(k-1)* R/(1-R)
