253864b8f134d7b85bb80956548aaf8e151e9644
sadamich <sadamichikato@gmail.com>
2026-05-12 06:04:19 +0900

    Moment ML

--- z_exercise_1_12.R
+++ z_exercise_1_12.R
@@ -14,9 +14,34 @@ return(result)
 }
 t(mean(FGPA), 2.79,sd(FGPA))
 [1] -0.1875647
+t.test(FGPA,mu=2.79)
+t.test(FGPA, mu = 2.79, alternative= "less")
+     One Sample t-test
+data:  FGPA
+t = -0.59313, df = 9, p-value = 0.2838
+alternative hypothesis: true mean is less than 2.79
+95 percent confidence interval:
+     -Inf 3.001565
+sample estimates:
+mean of x 
+   2.6888 
+### S.329
+t.test(FGPA)$conf.int
+[1] 2.302831 3.074769
+attr(,"conf.level")
+[1] 0.95
+### S. 331
 
 ### Problem (b)
-
+   One Sample t-test
+data:  FGPA
+t = -0.59313, df = 9, p-value = 0.5677
+alternative hypothesis: true mean is not equal to 2.79
+95 percent confidence interval:
+ 2.302831 3.074769
+sample estimates:
+mean of x 
+   2.6888 
 ### Problem (c)
 
 chi<- function(s_sq, sigma_s,n){
@@ -32,12 +57,20 @@ P-value
 1 - pchisq(12.38185, 9)
 [1] 0.1926282
 
+var(FGPA)
+[1] 0.2911111 (the sample variance) 
+0.46^2
+[1] 0.2116 (the thoretical variance)
+
 ### Problem (d)
 
-p<- 6/10
+beobH<- 6/10
 var(FEM)
-1/10*p*(1-p)
+1/609*p*(1-p)
 0.024
 
 
-236/609
+erwH<- 236/609
+residuum<- p - erwH
+cbind(p,erwH, residuum)
+prop.test(x = beobH, n= 10, p= 236/609, alternative="less")
