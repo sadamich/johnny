019ba8a62fcff39e6ec352e27989bda3cdf72379
sadamich <sadamichikato@gmail.com>
2026-05-15 06:17:52 +0900

    Mengen Set

---
+++ math_menge_set.R
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+https://de.wikipedia.org/wiki/Abgeschlossene_Menge
+
+
+
+https://de.wikipedia.org/wiki/Beschr%C3%A4nkte_Menge
+
+
+https://de.wikipedia.org/wiki/Offene_Menge
+
+
+Einheitsintervall 
+[0,1]⊂R{\displaystyle [0,1]\subset \mathbb {R} } 
