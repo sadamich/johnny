964b98033dc9fbe0d61a6fd22e32434fc1660880
sadamich <sadamichikato@gmail.com>
2026-05-16 06:19:21 +0900

    FGPA SATM

--- z_exercise_2_14.R
+++ z_exercise_2_14.R
@@ -20,17 +20,46 @@ SATM          0.5176     0.5035   1.028    0.334
 Residual standard error: 0.5379 on 8 degrees of freedom
 Multiple R-squared:  0.1167,    Adjusted R-squared:  0.006248 
 F-statistic: 1.057 on 1 and 8 DF,  p-value: 0.3341
-
+res<- resid(eq)
+ssr<- sum(res^2)
 ### Problem (c)
 confint(eq)
                  2.5 %   97.5 %
 (Intercept) -7.1095119 6.452117
 SATM        -0.6435615 1.678724
 
+sqrt(ssr/8/sum((SATM-mean(SATM))^2))
+[1] 0.5035301      sd(SATM)
+
+0.5176- 1.94*0.5035
+[1] -0.45919
+0.5176 + 1.94*0.5035
+[1] 1.49439
+
 ### Problem (d) forecast for students (SATM = 6)                          ###
+### full sample                                                           ###
 f<- -0.3287 +0.5176*SATM[SATM==6]
 f
 [1] 2.7769 2.7769
 FGPA[SATM==6]
 [1] 2.566 3.225
+u<- c(4,8)
+eq_8<- lm(FGPA[-u] ~ SATM[-u])
+summary(eq_8)
+Coefficients:
+            Estimate Std. Error t value Pr(>|t|)
+(Intercept) -0.08517    3.28648  -0.026    0.980
+SATM[-u]     0.47037    0.56673   0.830    0.438
+res_8<- resid(eq_8)
+ssr_8<- sum(res_8^2)
+ssr_8/8*(1+ 1/8+ (6- mean(SATM[-u])^2/sum(SATM[-u]- mean(SATM[-u])^2)))
+[1] 1.87322
 
+f_8<- -0.08517+0.47037*SATM[SATM==6]
+f_8
+[1] 2.73705 2.73705
+
+2.73705 - 1.94*sqrt( 1.87322)
+[1] 0.08185683
+2.73705 + 1.94*sqrt( 1.87322)
+5.392243
