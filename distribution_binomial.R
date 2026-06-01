c29a785b1754ca7743081c39634f205ba28d7252
sadamich <sadamichikato@gmail.com>
2026-06-02 07:42:56 +0900

    Binomial distribution

---
+++ distribution_binomial.R
@@ -0,0 +1,22 @@
+https://search.r-project.org/R/refmans/stats/html/Binomial.html
+
+Usage
+dbinom(x, size, prob, log = FALSE)
+pbinom(q, size, prob, lower.tail = TRUE, log.p = FALSE)
+qbinom(p, size, prob, lower.tail = TRUE, log.p = FALSE)
+rbinom(n, size, prob)
+
+
+
+para <- c(ja = 0.73, nein = 1 - 0.73)
+barplot(para)
+barplot(para, ylim = c(0,1), width = c(0.5, 0.5), xlim = c(0, 1.5))
+barplot(as.matrix(para), ylim = c(0,1), width = 0.2, xlim = c(0, 0.5),
+legend = names(para), main = "Glaube an paranormale Phaenomene")
+abline(h = 0.76, lty = 2, col = "blue", lwd = 1.5)
+
+beobH <- round(0.73*1008)
+prop.test(x = beobH, n = 1008, p = 0.76, alternative = "less", correct = FALSE)
+
+
+binom.test(x = beobH, n = 1008, p = 0.76, alternative = "less")
