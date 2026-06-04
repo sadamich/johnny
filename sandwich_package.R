415924aed412bcf1a998800e8100008258268fb8
sadamich <sadamichikato@gmail.com>
2026-06-05 06:14:43 +0900

    Sandwich

--- sandwich_package.R
+++ sandwich_package.R
@@ -6,20 +6,93 @@ Sandwich
 Bread    Hessian
 Meat     Gradient
 ### 3 2 Clustered variancies
-
-https://cran.r-project.org/web/packages/sandwich/refman/sandwich.html#sandwich
+library(sandwich)
+### https://cran.r-project.org/web/packages/sandwich/refman/sandwich.html#sandwich
 x <- sin(1:10)
 y <- rnorm(10)
 fm <- lm(y ~ x)
+
+### bread: n * (x'x)^{-1}
+bread(fm)
+            (Intercept)          x
+(Intercept)   1.0414689 -0.2938577
+x            -0.2938577  2.0823419
+
+solve(crossprod(cbind(1, x))) * 10
+                                 x
+              1.0414689 -0.2938577
+x            -0.2938577  2.0823419
+
+
+### estfun()                                                               ###
+## linear regression
+x <- 1:9
+y <- sin(1:9/5)
+m <- lm(y ~ x)
+## estimating function: (y - x'beta) * x
+estfun(m)
+ (Intercept)           x
+1 -0.13577304 -0.13577304
+2 -0.04481531 -0.08963062
+3  0.03061755  0.09185265
+4  0.08353989  0.33415956
+5  0.10786351  0.53931755
+6  0.09864034  0.59184202
+7  0.05225971  0.36581794
+8 -0.03340770 -0.26726157
+9 -0.15892494 -1.43032449
+residuals(m) * cbind(1, x)
+                           x
+ [1,] -0.13577304 -0.13577304
+ [2,] -0.04481531 -0.08963062
+ [3,]  0.03061755  0.09185265
+ [4,]  0.08353989  0.33415956
+ [5,]  0.10786351  0.53931755
+ [6,]  0.09864034  0.59184202
+ [7,]  0.05225971  0.36581794
+ [8,] -0.03340770 -0.26726157
+ [9,] -0.15892494 -1.43032449
+
+### Meat 
+x <- sin(1:10)
+y <- rnorm(10)
+fm <- lm(y ~ x)
+meat(fm)
+            (Intercept)         x
+(Intercept)   0.6761770 0.3064227
+x             0.3064227 0.2405474
+
+meatHC(fm, type = "HC")
+            (Intercept)         x
+(Intercept)   0.6761770 0.3064227
+x             0.3064227 0.2405474
+
+meatHAC(fm)
+            (Intercept)         x
+(Intercept)   0.7773621 0.4312438
+x             0.4312438 0.3694204
+ 
+
+### sandwich 
+x <- sin(1:10)
+y <- rnorm(10)
+fm <- lm(y ~ x)
+B<- bread(fm)
+M<- meat(fm)
+sw<- B%*%M%*%B
+sw
+            (Intercept)          x
+(Intercept)  0.73146773 0.03295184
+x            0.03295184 1.16217262
 sandwich(fm)
             (Intercept)           x
-(Intercept) 0.043265676 0.008950109
-x           0.008950109 0.079979393
+(Intercept) 0.073146773 0.003295184
+x           0.003295184 0.116217262
 
 vcovHC(fm, type = "HC")
             (Intercept)           x
-(Intercept) 0.043265676 0.008950109
-x           0.008950109 0.079979393
+(Intercept) 0.073146773 0.003295184
+x           0.003295184 0.116217262       
 
 https://cran.r-project.org/web/packages/sandwich/refman/sandwich.html#vcovHAC
 vcovHAC(x, ...)
