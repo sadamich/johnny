9b4a904f166aab1fa2e19842c45a191e88a85bf2
sadamich <sadamichikato@gmail.com>
2026-06-13 06:03:17 +0900

    LM test and repeating

--- z_exercise_4_10.R
+++ z_exercise_4_10.R
@@ -3,11 +3,13 @@
 ### Business and Economics. Oxford University Press                        ###
 ### https://global.oup.com/booksites/content/0199268010/                   ###
 ### Exercise 4 10 (p.269)                                                  ###
+set.seed(45)
 x<- rnorm(100, 0, 1)
 x_sq<- x^2
 w<- rnorm(100, 0, 1)
 y<- 1+x_sq+w
-
+plot(x, y)
+plot(x_sq,y)
 eq<- lm(y ~ x+x_sq)
 summary(eq)
 ### the unrestricted Model :lm(formula = y ~ x + x_sq)                      ###
@@ -39,23 +41,22 @@ Residual standard error: 0.9826 on 98 degrees of freedom
 Multiple R-squared:  0.6473,    Adjusted R-squared:  0.6437 
 F-statistic: 179.8 on 1 and 98 DF,  p-value: < 2.2e-16
 res_r<- resid(eq_r)
-
-eq_aux<- lm(res_r ~ x+x_sq)
+z<- 2*x
+eq_aux<- lm(res_r ~ z)
 summary(eq_aux)
-Call:lm(formula = res_r ~ x + x_sq)
+Call:lm(formula = res_r ~ z)
 Residuals:
-    Min      1Q  Median      3Q     Max 
--2.3091 -0.7772  0.0184  0.6431  3.1875 
+     Min       1Q   Median       3Q      Max 
+-2.66482 -0.69262  0.09193  0.76852  2.37461 
 Coefficients:
-             Estimate Std. Error t value Pr(>|t|)
-(Intercept)  0.004331   0.120291   0.036    0.971
-x           -0.038231   0.110050  -0.347    0.729
-x_sq        -0.006169   0.082000  -0.075    0.940
-Residual standard error: 0.9871 on 97 degrees of freedom
-Multiple R-squared:  0.001243,  Adjusted R-squared:  -0.01935 
-F-statistic: 0.06034 on 2 and 97 DF,  p-value: 0.9415
-
-LM = 100*0.001243=  0.1243
+            Estimate Std. Error t value Pr(>|t|)
+(Intercept)  0.00346    0.10187   0.034    0.973
+z           -0.02131    0.04502  -0.473    0.637
+Residual standard error: 1.016 on 98 degrees of freedom
+Multiple R-squared:  0.00228,   Adjusted R-squared:  -0.007901 
+F-statistic: 0.224 on 1 and 98 DF,  p-value: 0.6371
+0.00228*100
+LM = 100*0.00228=  0.228
 ### H0 (b2=0) is not rejected.
 no_sample<- c(10, 100)
 eq<- lm(y~ x+x_sq)
