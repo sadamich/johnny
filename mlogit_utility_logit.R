4c52158e7d2dd02b573fa9d2a4e5805278113dc1
sadamich <sadamichikato@gmail.com>
2026-06-23 06:21:31 +0900

    MNL

--- mlogit_utility_logit.R
+++ mlogit_utility_logit.R
@@ -26,18 +26,39 @@ ml.MC1 <- mlogit(choice ~ cost + freq | income | time, MC,
                  alt.subset = c("car", "train", "air"),
                  reflevel = "car")
 
-
 summary(ml.MC1)
-
-
 head(fitted(ml.MC1, type = "outcome"))
-
-
 head(fitted(ml.MC1, type = "probabilities"), 4)
-
-
 sum(log(fitted(ml.MC1, type = "outcome")))
-
 apply(fitted(ml.MC1, type = "probabilities"), 2, mean)
-
 head(predict(ml.MC1, shape = "wide"))
+
+### The modificatin of the model
+NMC <- MC
+NMC$time[NMC$alt == "train"] <- 0.8 * NMC$time[NMC$alt == "train"]
+Oprob <- fitted(ml.MC1, type = "probabilities")
+Nprob <- predict(ml.MC1, newdata = NMC, shape = "wide")
+rbind(old = apply(Oprob, 2, mean), new = apply(Nprob, 2, mean))
+head(Nprob[, "air"] / Nprob[, "car"])
+head(Oprob[, "air"] / Oprob[, "car"])
+### The surplus (der Ueberschuss) 
+ivbefore <- logsum(ml.MC1)
+ivafter <- logsum(ml.MC1, data = NMC)
+surplus <- - (ivafter - ivbefore) / coef(ml.MC1)["cost"]
+summary(surplus)
+effects(ml.MC1, covariate = "income", type = "ar")
+effects(ml.MC1, covariate = "cost", type = "rr")
+coef(ml.MC1)[grep("time", names(coef(ml.MC1)))] /coef(ml.MC1)["cost"] * 60 
+
+
+### Predictions and marginal effects
+
+ids <- unique(ModeCanada$case)
+set.seed(1L)
+ids <- sample(ids, 50)
+smallMC <- subset(ModeCanada, case %in% ids)
+ml <- mlogit(choice ~ cost | income, smallMC, alt.subset = c("car", "train", "air"))
+summary(ml)
+
+p<- preds(ml)
+
