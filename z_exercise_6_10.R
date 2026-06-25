ef2f24872c59215744275aab3027663cee4c6924
sadamich <sadamichikato@gmail.com>
2026-06-26 04:44:03 +0900

    Ex

---
+++ z_exercise_6_10.R
@@ -0,0 +1,22 @@
+### Source:Christiaan Heij, Paul de Boer, Philip Hans Franses, Teun Kloek, ###
+### Herman K. van Dijk (2004).Econometric Methods with Applications in     ###
+### Business and Economics. Oxford University Press                        ###
+### https://global.oup.com/booksites/content/0199268010/                   ###
+### Exercise 6 10 (p. 527) Teleworking
+
+xr610<- read.csv("xr610.csv", header=TRUE)
+str(xr610)
+attach(xr610)
+
+### Problem (a) The Logit (H1) model: the hit rate 
+
+### Problem (b) The Logit (H0) model + additional variabe: the hit rate
+
+### Problem (c) The reason for the difference
+
+### Problem (d) The LR test: H0 model vs H1 model
+
+### Problem (e) The distance preference of females and of males
+
+
+detach(xr610)
