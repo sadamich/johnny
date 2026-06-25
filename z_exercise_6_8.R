7625ef2659a4cf5b7e05533966968d8f5dba73ef
sadamich <sadamichikato@gmail.com>
2026-06-25 14:20:11 +0900

    Probit Logit Hetero

---
+++ z_exercise_6_8.R
@@ -0,0 +1,25 @@
+### Source:Christiaan Heij, Paul de Boer, Philip Hans Franses, Teun Kloek, ###
+### Herman K. van Dijk (2004).Econometric Methods with Applications in     ###
+### Business and Economics. Oxford University Press                        ###
+### https://global.oup.com/booksites/content/0199268010/                   ###
+### Exercise 6 8 (p. 527)
+
+x<- rnorm(10, 100, 100)
+e<- rnorm(10, 0, 1)
+y<- -10 + 0.1*x+e
+
+
+x2<- rnorm(100, 100, 100)
+e2<- rnorm(100, 0, 1)
+y2<- -10 + 0.1*x2+e2
+
+x3<- rnorm(1000, 100, 100)
+e3<- rnorm(1000, 0, 1)
+y3<- -10 + 0.1*x3+e3
+
+### Problem (a) The Probit model 
+
+### Problem (b) The Logit model
+
+### Problem (c) The heteroskedasticity
+
