e2dd1221d0cb3b5f727f9258c5e223c423e99e52
sadamich <sadamichikato@gmail.com>
2026-06-26 04:46:11 +0900

    Ex

---
+++ z_exercise_6_11.R
@@ -0,0 +1,23 @@
+### Source:Christiaan Heij, Paul de Boer, Philip Hans Franses, Teun Kloek, ###
+### Herman K. van Dijk (2004).Econometric Methods with Applications in     ###
+### Business and Economics. Oxford University Press                        ###
+### https://global.oup.com/booksites/content/0199268010/                   ###
+### Exercise 6 11 (p. 528) Marketing 
+
+xm601<- read.csv("xm601.csv", header=TRUE)
+str(xm601)
+attach(xm601)
+### Problem (a) The Logit model
+
+### Problem (b) The Probit model
+
+### Problem (c) 
+
+### Problem (d) 
+
+
+
+
+
+
+detach(xm601)
