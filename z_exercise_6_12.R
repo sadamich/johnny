0e04c085fb28bad9b00e00e29578d747bf5da995
sadamich <sadamichikato@gmail.com>
2026-06-26 04:48:42 +0900

    Ex

---
+++ z_exercise_6_12.R
@@ -0,0 +1,25 @@
+### Source:Christiaan Heij, Paul de Boer, Philip Hans Franses, Teun Kloek, ###
+### Herman K. van Dijk (2004).Econometric Methods with Applications in     ###
+### Business and Economics. Oxford University Press                        ###
+### https://global.oup.com/booksites/content/0199268010/                   ###
+### Exercise 6 12 (p. 528) Marketing: The Group 
+
+xm601<- read.csv("xm601.csv", header=TRUE)
+str(xm601)
+attach(xm601)
+### Problem (a) 10 Groups  The Logit model
+
+### Problem (b) The Logit model
+
+### Problem (c) The Logit model with restrictions
+
+### Problem (d) The Logit model with FWLS estimate
+
+### Problem (e) Comparison with b and d
+
+
+
+
+
+
+detach(xm601)
