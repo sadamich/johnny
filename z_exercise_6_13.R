1ce78d3db7107514fec6aa996f4967fca5c85e1c
sadamich <sadamichikato@gmail.com>
2026-06-27 06:44:36 +0900

    Ex

---
+++ z_exercise_6_13.R
@@ -0,0 +1,25 @@
+### Source:Christiaan Heij, Paul de Boer, Philip Hans Franses, Teun Kloek, ###
+### Herman K. van Dijk (2004).Econometric Methods with Applications in     ###
+### Business and Economics. Oxford University Press                        ###
+### https://global.oup.com/booksites/content/0199268010/                   ###
+### Exercise 6 13 (p. 528)  Bank wages
+
+xr613<- read.csv("xr613.csv", header=TRUE)
+str(xr613)
+attach(xr613)
+
+
+### Problem (a) Logit model
+
+### Problem (b) Marginal effects
+
+### Problem (c) LR test: H0 model vs H1 Model: the heteroskedasticity
+
+### Problem (d) McFadden R_sq: hit rate
+
+### Problem (e) Compare with binary logit model and the multinomial logit model
+
+### Problem (f) Logit, multinomial, ordered Logit model
+
+
+detach(xr613)
