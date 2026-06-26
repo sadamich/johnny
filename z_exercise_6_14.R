f449951c73b9d76cba0c4353c75509c9e6014da9
sadamich <sadamichikato@gmail.com>
2026-06-27 06:45:13 +0900

    Ex

---
+++ z_exercise_6_14.R
@@ -0,0 +1,26 @@
+### Source:Christiaan Heij, Paul de Boer, Philip Hans Franses, Teun Kloek, ###
+### Herman K. van Dijk (2004).Econometric Methods with Applications in     ###
+### Business and Economics. Oxford University Press                        ###
+### https://global.oup.com/booksites/content/0199268010/                   ###
+### Exercise 6 14 (p. 529)   Bank wages
+
+xr614<- read.csv("xr614.csv", header=TRUE)
+str(xr614)
+attach(xr614)
+### Problem (a) MNL model
+
+### Problem (b) the coefficient = - Inf, describe a practical method
+
+### Problem (c) the mixed model
+
+### Problem (d) the comparison of b and c
+
+### Problem (e) the diagnostic
+
+
+
+
+
+
+
+detach(xr614)
