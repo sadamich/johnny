7eee4307daba74a99591295e9a53be28c028dcc4
sadamich <sadamichikato@gmail.com>
2026-06-28 06:05:52 +0900

    Ex

---
+++ z_exercise_6_15.R
@@ -0,0 +1,19 @@
+### Source:Christiaan Heij, Paul de Boer, Philip Hans Franses, Teun Kloek, ###
+### Herman K. van Dijk (2004).Econometric Methods with Applications in     ###
+### Business and Economics. Oxford University Press                        ###
+### https://global.oup.com/booksites/content/0199268010/                   ###
+### Exercise 6 15 (p. 529)   Microeconomics
+
+xr615<- read.csv("xr615.csv", header=TRUE)
+str(xr615)
+attach(xr615)
+### Problem (a) the ordered Logit model 
+
+### Problem (b) the comparison of the ordered logit with the probit model
+
+### Problem (c) the MNL mode
+
+
+
+
+detach(xr615)
