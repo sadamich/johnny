2baee6773c3097737bd8b8f5a218219d2cbe1351
sadamich <sadamichikato@gmail.com>
2026-06-28 13:59:12 +0900

    The censored model and odds ratio

--- z_exercise_6_7.R
+++ z_exercise_6_7.R
@@ -2,11 +2,21 @@
 ### Herman K. van Dijk (2004).Econometric Methods with Applications in     ###
 ### Business and Economics. Oxford University Press                        ###
 ### https://global.oup.com/booksites/content/0199268010/                   ###
-
+set.seed(44)
 n<- 200
 x<- 1:200
-set.seed(75)
 e<- dnorm(200,0,1)
-y_star<- -10+0.1*x+e
+y<- -10+0.1*x+e
+plot(y)
+### Problem (a) the censored model ###
 
-y<- c(1 if y_star >= 0  and 0 if y_star <0 )
+### Problem (b) the odds ratio 
+
+### Problem (c) the regression and the odds ratio
+
+### Problem (d) the probit model
+
+### Problem (e) the logit model 
+
+### Problem (f) the logit model and the odds ratio
+
