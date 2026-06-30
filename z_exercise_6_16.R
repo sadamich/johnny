e74d3ee016f86c015f98d0306b6afab97b67de25
sadamich <sadamichikato@gmail.com>
2026-07-01 05:50:16 +0900

    Ex

---
+++ z_exercise_6_16.R
@@ -0,0 +1,17 @@
+### Source:Christiaan Heij, Paul de Boer, Philip Hans Franses, Teun Kloek, ###
+### Herman K. van Dijk (2004).Econometric Methods with Applications in     ###
+### Business and Economics. Oxford University Press                        ###
+### https://global.oup.com/booksites/content/0199268010/                   ###
+### Exercise 6 16 (p. 530)   Marketing
+
+xm601<- read.csv("xm601.csv", header=TRUE)
+str(xm601)
+attach(xm601)
+
+### Problem (a) the comparison of the censored with the binary probit model
+
+
+### Problem (b) better methods?
+
+detach(xm601)
+
