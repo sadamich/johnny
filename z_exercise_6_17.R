c56a0cc4e04badb5f7cd1e9d6a8db920a17b5fdf
sadamich <sadamichikato@gmail.com>
2026-07-01 05:52:11 +0900

    Ex

---
+++ z_exercise_6_17.R
@@ -0,0 +1,26 @@
+### Source:Christiaan Heij, Paul de Boer, Philip Hans Franses, Teun Kloek, ###
+### Herman K. van Dijk (2004).Econometric Methods with Applications in     ###
+### Business and Economics. Oxford University Press                        ###
+### https://global.oup.com/booksites/content/0199268010/                   ###
+### Exercise 6 17 (p. 530)   Strike
+
+xm609<- read.csv("xm609.csv", header=TRUE)
+str(xm609)
+attach(xm609)
+
+### Problem (a) the uniform distribution: the survival values
+
+### Problem (b) the generalized residuals: an exponential distribution
+
+### Problem (c) the generalized residuals: censored model
+
+### Problem (d) the hazard rate
+
+### Problem (e) the first three sample moments: the generalized residuals
+
+### Problem (f) the hazard rate
+
+
+
+
+detach(xm609)
