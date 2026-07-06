92f0fa01749ad5c43e9e71aa5c8498a143ec8327
sadamich <sadamichikato@gmail.com>
2026-07-07 05:33:30 +0900

    Groups Marketing

--- z_exercise_6_12.R
+++ z_exercise_6_12.R
@@ -7,6 +7,16 @@
 xm601<- read.csv("xm601.csv", header=TRUE)
 str(xm601)
 attach(xm601)
+group1<- subset(xm601, AGE<=30)
+group2<- subset(xm601, 30< AGE <=35)
+group3<- subset(xm601, AGE > 35, AGE <=40)
+group4<- subset(xm601, AGE > 40, AGE <= 45)
+group5<- subset(xm601, AGE > 45, AGE <= 50)
+group6<- subset(xm601, AGE > 50, AGE <= 55)
+group7<- subset(xm601, AGE > 55, AGE <= 60)
+group8<- subset(xm601, AGE > 60, AGE <= 65)
+group9<- subset(xm601, AGE > 65, AGE <= 70)
+group10<- subset(xm601, AGE > 70, AGE <= 75)
 ### Problem (a) 10 Groups  The Logit model
 
 ### Problem (b) The Logit model
