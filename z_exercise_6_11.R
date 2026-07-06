abd577e3b4531aaffe83bb34a35c73cf7ffb5f47
sadamich <sadamichikato@gmail.com>
2026-07-07 05:32:24 +0900

    Logit Probit Marketing

--- z_exercise_6_11.R
+++ z_exercise_6_11.R
@@ -8,13 +8,23 @@ xm601<- read.csv("xm601.csv", header=TRUE)
 str(xm601)
 attach(xm601)
 ### Problem (a) The Logit model
+eq_logit<- glm(formula = RESPONSE ~ GENDER + ACTIVITY + AGE + AGE_2, 
+family = binomial)
+summary(eq_logit)
+p_fit<- fitted(eq_logit)
+plot(p_fit)
+plot(GENDER[GENDER==1],p_fit[GENDER==1])
 
+y<- RESPONSE
+y_active<- RESPONSE[GENDER==1]and[ACTIVITY==1]
 ### Problem (b) The Probit model
-
+eq_probit<- glm(formula = RESPONSE ~ GENDER + ACTIVITY + AGE + AGE_2, 
+family = binomial(link = "probit"))
+summary(eq_probit)
+p_fit_pro<- fitted(eq_probit)
 ### Problem (c) 
 
-### Problem (d) 
-
+### Problem (d) Random sampling
 
 
 
