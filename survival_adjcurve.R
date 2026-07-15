63fce179e30efcbbae007412eaf81abecd63b8c8
sadamich <sadamichikato@gmail.com>
2026-07-16 07:37:34 +0900

    Survival function

---
+++ survival_adjcurve.R
@@ -0,0 +1,15 @@
+https://cran.r-project.org/web/packages/survival/vignettes/adjcurve.pdf
+
+
+###  marginal analysis
+
+### understand and model the effect of each confounder:the conditional approach
+
+###  division of the covariates x into effects of interest vs.confounders
+
+
+Marginal: balance data on x −→ form survival curves for each c
+Conditional:predicted curves for {x,c}subset→average the predictions for each c
+
+
+### 2 Free Light Chain
