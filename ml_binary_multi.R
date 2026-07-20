f2c988eba3e4f882be0b48475274ed7e92c0fc2b
sadamich <sadamichikato@gmail.com>
2026-07-20 15:50:38 +0900

    MNL

--- ml_binary_multi.R
+++ ml_binary_multi.R
@@ -8,6 +8,7 @@ attach(xm604)
 str(xm604)
 library(maxLik)
 y<- JOBCAT[GENDER==1]
+y1<- DUMJCAT1[GENDER==1]
 y2<- DUMJCAT2[GENDER==1]
 y3<- DUMJCAT3[GENDER==1]
 const<- rep(1, 258)
@@ -15,16 +16,26 @@ p<- exp(beta1*const+beta2*EDUC+beta3*MINORITY)/sum(exp(beta1*const+beta2*EDUC+be
 
 
 l<- function(beta){
-beta2_1<- beta[1]
-beta2_2<- beta[2]
-beta2_3<- beta[3]
-beta3_1<- beta[4]
-beta3_2<- beta[5]
-beta3_3<- beta[6]
-y2*beta2_1*const+y2*beta2_2*EDUC+y2*beta2_3*MINORITY
+beta1_1<- beta[1]
+beta1_2<- beta[2]
+beta1_3<- beta[3]
+beta2_1<- beta[4]
+beta2_2<- beta[5]
+beta2_3<- beta[6]
+beta3_1<- beta[7]
+beta3_2<- beta[8]
+beta3_3<- beta[9]
+beta1_1<- 0
+beta1_2<- 0
+beta1_3<- 0
+sum(
+    y1*beta1_1*const+y1*beta1_2*EDUC+y1*beta1_3*MINORITY 
+   +y2*beta2_1*const+y2*beta2_2*EDUC+y2*beta2_3*MINORITY
    +y3*beta3_1*const+y3*beta3_2*EDUC+y3*beta3_3*MINORITY
  - log(1+exp(beta2_1*const+beta2_2*EDUC+beta2_3*MINORITY
                  +beta3_1*const+beta3_2*EDUC+beta3_3*MINORITY))
+)
 }
-m<- maxLik(l, start= c(0,0,0,0,0,0))
+m<- maxLik(l, start= c(0,0,0,4.7,-0.5,0.4,-26,1.6,-2))
 summary(m)
+????
