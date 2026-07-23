bdefa0799ffc9ca918262aba1ac108a8b9d93b75
sadamich <sadamichikato@gmail.com>
2026-07-24 06:41:05 +0900

    Package

--- survival_package.R
+++ survival_package.R
@@ -1,11 +1,50 @@
 https://cran.r-project.org/web/packages/survival/refman/survival.html#survreg
 library("survival")
 
-# Fit an exponential model: the two fits are the same
-survreg(Surv(futime, fustat) ~ ecog.ps + rx, ovarian, dist='weibull',
+### Fit an exponential model: the two fits are the same                ###
+s<- survreg(Surv(futime, fustat) ~ ecog.ps + rx, ovarian, dist='weibull',
                                     scale=1)
-survreg(Surv(futime, fustat) ~ 1, ovarian, dist='weibull',
+summary(s)
+### Call:survreg(formula = Surv(futime, fustat) ~ ecog.ps + rx, data = ovarian, 
+    dist = "weibull", scale = 1)
+             Value Std. Error     z       p
+(Intercept)  6.962      1.322  5.27 1.4e-07
+ecog.ps     -0.433      0.587 -0.74    0.46
+rx           0.582      0.587  0.99    0.32
+Scale fixed at 1 
+Weibull distribution
+Loglik(model)= -97.2   Loglik(intercept only)= -98
+        Chisq= 1.67 on 2 degrees of freedom, p= 0.43 
+Number of Newton-Raphson Iterations: 4 
+n= 26 
+
+s1<- survreg(Surv(futime, fustat) ~ 1, ovarian, dist='weibull',
                                     scale=1)
+summary(s1)
+### Call: survreg(formula = Surv(futime, fustat) ~ 1, data = ovarian, dist = "weibull", 
+    scale = 1)
+            Value Std. Error    z      p
+(Intercept) 7.169      0.289 24.8 <2e-16
+Scale fixed at 1 
+Weibull distribution
+Loglik(model)= -98   Loglik(intercept only)= -98
+Number of Newton-Raphson Iterations: 4 
+n= 26 
+### the scale is 0                                                        ###
+s2<- survreg(Surv(futime, fustat) ~ 1, ovarian, dist='weibull',
+                                    scale=0)
+summary(s2)
+### survreg(formula = Surv(futime, fustat) ~ 1, data = ovarian,
+### dist = "weibull", cale = 0)
+             Value Std. Error    z      p
+(Intercept)  7.111      0.293 24.3 <2e-16
+Log(scale)  -0.103      0.254 -0.4   0.69
+Scale= 0.902 
+Weibull distribution
+Loglik(model)= -98   Loglik(intercept only)= -98
+Number of Newton-Raphson Iterations: 5 
+n= 26 
+
 ### futime:	survival or censoring time
 ### fustat:	censoring status
 ### age:	in years
