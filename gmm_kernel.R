f18f83b737eb1b74d1f41b25e9300d718109fd14
sadamich <sadamichikato@gmail.com>
2026-07-28 09:12:56 +0900

    GMM and Kernel

---
+++ gmm_kernel.R
@@ -0,0 +1,27 @@
+https://cran.r-project.org/web/packages/gmm/refman/gmm.html#smoothG
+library(gmm)
+
+Usage
+smoothG(x, bw = bwAndrews, prewhite = 1, ar.method = "ols", weights = weightsAndrews,
+	kernel = c("Bartlett", "Parzen", "Truncated", "Tukey-Hanning"), 
+	approx = c("AR(1)", "ARMA(1,1)"), tol = 1e-7) 
+
+
+
+g <- function(tet, x)
+	{
+	n <- nrow(x)
+	u <- (x[7:n] - tet[1] - tet[2]*x[6:(n-1)] - tet[3]*x[5:(n-2)])
+	f <- cbind(u, u*x[4:(n-3)], u*x[3:(n-4)], u*x[2:(n-5)], u*x[1:(n-6)])
+	return(f)
+	}
+n = 500
+phi<-c(.2, .7)
+thet <- 0.2
+sd <- .2
+x <- matrix(arima.sim(n = n, list(order = c(2, 0, 1), ar = phi, ma = thet, sd = sd)), ncol = 1)
+gt <- g(c(0, phi), x) 
+sgt <- smoothG(gt)$smoothx
+
+plot(gt[,1])
+lines(sgt[,1])
