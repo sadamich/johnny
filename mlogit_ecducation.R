56cf38f30cdc8eed06ecdd94a7d0c8c451b6c8af
sadamich <sadamichikato@gmail.com>
2026-07-28 09:11:28 +0900

    MNL

--- mlogit_ecducation.R
+++ mlogit_ecducation.R
@@ -29,58 +29,19 @@ ED <- dfidx(data_ed,shape= "wide", varying = 3:8, sep ="_",
 )
 m<- mlogit(JOBCAT~ 1|EDUC + MINORITY , ED)
 summary(m)
-jobcat<- factor(JOBCAT, levels = c("1","2","3"))
-str(jobcat)
-Call:
-mlogit(formula = JOBCAT ~ 1 | EDUC | MINORITY, data = ED, method = "nr")
+noalt<- rep(3, 258)
+jobcat<- dfidx(data_s, subset = noalt == 3, alt.levels = c("1", "2","3"))
 
-Frequencies of alternatives:choice
-      1       2       3 
-0.33333 0.33333 0.33333 
-
-nr method
-1 iterations, 0h:0m:0s 
-g'(-H)^-1g = 1E+10 
-successive function values within tolerance limits 
-
-Coefficients :
-              Estimate Std. Error z-value Pr(>|z|)
-(Intercept):2 0.000000   0.734483       0        1
-(Intercept):3 0.000000   0.739815       0        1
-EDUC:2        0.000000   0.054699       0        1
-EDUC:3        0.000000   0.017369       0        1
-MINORITY:1    0.000000   0.797108       0        1
-MINORITY:2    0.000000   0.783476       0        1
-MINORITY:3    0.000000   1.064001       0        1
-
-Log-Likelihood: 7.7716e-16
-McFadden R^2:  -Inf 
-Likelihood ratio test : chisq = 1700.7 (p.value = < 2.22e-16)
-> 
-
-
-method = c("bfgs", "nr", "bhhh")
-nojob<- rep(3, 258)
-data_s<- data.frame(OBS,JOBCAT,EDUC,MINORITY,nojob)
-ED<- dfidx(data_s)
-attach(ED)
-str(JOBCAT)
 library("Formula")
 f<- Formula(JOBCAT ~ EDUC | MINORITY)
-mf<- model.frame(ED,f)
+mf<- model.frame(jobcat,f)
 md<- mlogit.data(data_s, JOBCAT, EDUC, MINORITY, shape = "wide", sep= FALSE)
 ????
 head(ED, 10)
 
-xm604_s$OBS<- 1:nrow(xm604_s)
-head(xm604_s,5)
-newdata<- data.frame(OBS,DUMJCAT2,DUMJCAT3,DUMJCAT1,EDUC,MINORITY, JOBCAT)
-head(newdata, 5)
-JOBCAT<- factor(JOBCAT, labels=c("1","2","3"))
-MC<- dfidx(newdata,drop.index=FALSE)
-head(MC,5)
-eqx<- mlogit(DUMJCAT2~EDUC+MINORITY,MC)
-summary(eqx)
+
+m<- mlogit(JOBCAT~1|EDUC|MINORITY,mf)
+summary(m)
 ### Indexing                                                               ###
 idx<- as.numeric(rownames(xm604))
 JOBCAT<- JOBCAT[idx]
@@ -91,59 +52,6 @@ GENDER<- GENDER[idx]
 MINORITY<- MINORITY[idx]
 
 
-### Subset GENDER == 1                                                     ###
-JOBCAT<- JOBCAT[GENDER==1]
-EDUC<- EDUC[GENDER==1]
-MINORITY<- MINORITY[GENDER==1]
-DUMJCAT1<- DUMJCAT1[GENDER==1]
-DUMJCAT2<- DUMJCAT2[GENDER==1]
-DUMJCAT3<- DUMJCAT3[GENDER==1]
-JOBCAT_f<- factor(JOBCAT,labels=c("1","2","3"))
-OBS<- OBS[GENDER==1]
-job_s<- data.frame(DUMJCAT1,DUMJCAT2,DUMJCAT3,EDUC,MINORITY,JOBCAT_f,OBS)
-head(job_s,5)
-  DUMJCAT1 DUMJCAT2 DUMJCAT3 EDUC MINORITY JOBCAT_f OBS
-1        0        0        1   15        0        3   1
-2        1        0        0   16        0        1   2
-3        1        0        0   15        1        1   7
-4        1        0        0    8        0        1  12
-5        1        0        0   15        0        3  13
-
-job_m<- mlogit.data(job_s, choice = c("DUMJCAT1","DUMJCAT2","DUMJCAT3"),
-shape= "long")
-            
-JOBCAT_f<- JOBCAT[idx]
-DUMJCAT2<- DUMJCAT2[idx]
-job_m<- data.frame(DUMJCAT1,DUMJCAT2,DUMJCAT3,EDUC,MINORITY, JOBCAT_f)
-MC <- dfidx(job_s, subset = JOBCAT_f == 3, idx=c("JOBCAT_f","DUMJCAT2"))
-job_mlogit<- mlogit(DUMJCAT2~ EDUC+MINORITY,xm604_i, JOBCAT_f)
-summary(eq)
-mnl<- function(theta){
-beta2_1<- theta[1]
-beta2_2<- theta[2]
-beta2_3<- theta[3]
-beta3_1<- theta[4]
-beta3_2<- theta[5]
-beta3_3<- theta[6]
-sum(JOBCAT*(beta2_1+beta2_2*EDUC+beta2_3*MINORITY)
-   +JOBCAT*(beta3_1+beta3_2*EDUC+beta2_3*MINORITY)
-   - log(1+ exp(beta2_1+beta2_2*EDUC+beta2_3*MINORITY)
-      +exp(beta3_1+beta3_2*EDUC+beta2_3*MINORITY)))
-}
-library(maxLik)
-m_mnl<- maxLik(mnl, start= c(0,1,1,0,1,1))
-summary(m_mnl)
-co <- maxControl(printLevel=2, qac="marquardt", marquardt_lambda0=1)
-eq <- maxNR(mnl, start=c(1,1,1,1,1,1), control=co)
-summary(eq)
-
-
-data_m<- mlogit.data(xm604, choice=NULL,alt.var=JOBCAT,
-
-str(data_m)
-head(data_m,5)
-head(data_m, 50:55)
-library(mlogit)
 ### Wide
 xm604$JOBCAT <- 1:nrow(xm604)
 head(xm604,3)
@@ -164,16 +72,5 @@ library("Formula")
 JOBCAT<- as.factor(JOBCAT)
 f<- formula(JOBCAT ~ EDUC |MINORITY)
 
-eq1<- mlogit(f,data)
-summary(eq1)
 
 
-MC <- dfidx(ModeCanada, subset = noalt == 4,
-            alt.levels = c("train", "air", "bus", "car"))
-
-f <- Formula(choice ~ cost | income + urban | ivt)
-f2 <- Formula(choice ~ cost + ivt | income + urban)
-f2 <- Formula(choice ~ cost + ivt | income + urban | 0)
-f3 <- Formula(choice ~ 0 | income | 0)
-eq_ex<- mlogit(f, MC)
-summary(eq_ex)
