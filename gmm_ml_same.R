36b50e5e66248a27977d44c4bd0241734b79f95f
sadamich <sadamichikato@gmail.com>
2026-08-04 06:16:02 +0900

    GMM ML

--- gmm_ml_same.R
+++ gmm_ml_same.R
@@ -1,3 +1,10 @@
+### Source:Christiaan Heij, Paul de Boer, Philip Hans Franses, Teun Kloek, ###
+### Herman K. van Dijk (2004).Econometric Methods with Applications in     ###
+### Business and Economics. Oxford University Press                        ###
+### https://global.oup.com/booksites/content/0199268010/                   ###
+###
+
+
 library(gmm)
 ### The GMM estimation                                                     ###
 g1 <- function(tet,x)
