490413e73ef318739cd528ea1e29cd9b6ce4f399
sadamich <sadamichikato@gmail.com>
2026-08-05 08:42:29 +0900

    GMM OLS ML

--- gmm_ml_same_ols.R
+++ gmm_ml_same_ols.R
@@ -37,7 +37,50 @@ x            2.2838e+00  1.2264e-01  1.8623e+01  2.0971e-77
 J-Test: degrees of freedom is 0 
                 J-test                P-value             
 Test E(g)=0:    7.85207119265002e-31  ******* 
+library(sandwich)
+sandwich(eq_gmm)
+            [,1]        [,2]
+[1,] 0.027642036 0.005159768
+[2,] 0.005159768 0.017977287
+bread(eq_gmm)
+          [,1]      [,2]
+[1,] 1.2780984 0.1072541
+[2,] 0.1072541 0.4511909
+meat(eq_gmm)
+            [,1]        [,2]
+[1,]  0.50104347 -0.07205672
+[2,] -0.07205672  2.65520576
+
+
+
+
 ### GMM = OLS                                                              ###
+
+### ML estimate                                                            ###
+ml<- function(theta){
+beta1 <- theta[1]
+beta2 <- theta[2]
+sigma <- theta[3]
+mu<- beta1 + beta2*x
+u <- y - mu
+N<- 30
+ -N/2*log(2*pi) - N/2*log(sigma^2) - 1/(2*sigma^2)*sum(u^2)
+}
+library(maxLik)
+m<- maxLik(ml, start = c(0,0,1))
+summary(m)
+Maximum Likelihood estimation
+Newton-Raphson maximisation, 8 iterations
+Return code 1: gradient close to zero (gradtol)
+Log-Likelihood: -39.17407 
+3  free parameters
+Estimates:
+     Estimate Std. error t value Pr(> t)    
+[1,]   0.1659     0.1646   1.008   0.314    
+[2,]   2.2838     0.1710  13.355 < 2e-16 ***
+[3,]   0.8930     0.1153   7.746 9.5e-15 ***
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1
+### GMM = ML = OLS                                                         ###
 
 
 ?????????
