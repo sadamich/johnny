495f53f849bddf395fb1080c9b794020b030e332
sadamich <sadamichikato@gmail.com>
2026-08-07 13:21:27 +0900

    Hazard models

---
+++ survival_hazard_rate.R
@@ -0,0 +1,10 @@
+### Source:Christiaan Heij, Paul de Boer, Philip Hans Franses, Teun Kloek, ###
+### Herman K. van Dijk (2004).Econometric Methods with Applications in     ###
+### Business and Economics. Oxford University Press                        ###
+### https://global.oup.com/booksites/content/0199268010/                   ###
+
+t= exp(-sigma^2+1/2sigma+mu
+ = exp(1.5) | sigma = 1, mu = 2
+### Exhibit 6 15 (d) (p.514) 
+dnorm(-0.5)/(exp(1.5)*(1-pnorm(-0.5)))
+[1] 0.113609
