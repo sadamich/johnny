33e577223b8e6f49ef34f11470bc0120f60fdd2b
sadamich <sadamichikato@gmail.com>
2026-08-08 10:12:00 +0900

    Hazard rate

--- survival_hazard_rate.R
+++ survival_hazard_rate.R
@@ -2,6 +2,22 @@
 ### Herman K. van Dijk (2004).Econometric Methods with Applications in     ###
 ### Business and Economics. Oxford University Press                        ###
 ### https://global.oup.com/booksites/content/0199268010/                   ###
+### Exhibit 6 15 (b) (p.514) 
+t<- 1:80
+lambda_weibullb<- function(t){
+result <- 1.5*t^(1.5-1)
+return(result)
+}
+z<- lambda_weibullb(t)
+plot(z, tyep="l")
+### Exhibit 6 15 (c) (p.514) 
+lambda_weibullc<- function(t){
+result <- 0.5*t^(0.5-1)
+return(result)
+}
+v<- lambda_weibullc(t)
+plot(v, tyep="l")
+
 
 t= exp(-sigma^2+1/2sigma+mu
  = exp(1.5) | sigma = 1, mu = 2
