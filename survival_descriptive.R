a85ef9486b86e8517b2400ae3825dae61f0a7228
sadamich <sadamichikato@gmail.com>
2026-08-12 20:12:48 +0900

    Duration

---
+++ survival_descriptive.R
@@ -0,0 +1,10 @@
+### Source:Christiaan Heij, Paul de Boer, Philip Hans Franses, Teun Kloek, ###
+### Herman K. van Dijk (2004).Econometric Methods with Applications in     ###
+### Business and Economics. Oxford University Press                        ###
+### https://global.oup.com/booksites/content/0199268010/                   ###
+xm609<- read.csv("xm609.csv", header = TRUE)
+attach(xm609)
+
+x<- STRIKEDUR
+
+hist(x)
