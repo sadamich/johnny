2188a2029716d5f06d9c39fe91bdd90f75cea6a4
sadamich <sadamichikato@gmail.com>
2026-08-12 20:31:56 +0900

    Duration

--- survival_descriptive.R
+++ survival_descriptive.R
@@ -8,3 +8,23 @@ attach(xm609)
 x<- STRIKEDUR
 
 hist(x)
+summary(x)
+
+   Min.  1st Qu.  Median   Mean  3rd Qu.    Max. 
+   1.00   10.25   27.00   42.68   51.25  216.00
+
+x1<- x[x<=10.25]
+str(x1)
+int [1:16] 1 2 2 2 3 3 3 3 3 4 ...
+x2<- x[x<=27]
+str(x2)
+int [1:32] 1 2 2 2 3 3 3 3 3 4 ...
+x3<- x[x<=51.25]
+str(x3)
+ int [1:46] 1 2 2 2 3 3 3 3 3 4 .
+x4<- x[x<=216]
+str(x4)
+int [1:62] 1 2 2 2 3 3 3 3 3 4 ...
+quan<- c(x1,x2,x3,x4)
+q<- c(16,16,14,16)
+plot(q)
