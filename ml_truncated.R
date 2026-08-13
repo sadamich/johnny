28fdeadb018bd17041ce9bc89a53f51a11563bee
sadamich <sadamichikato@gmail.com>
2026-08-13 13:47:50 +0900

    Truncated Model

--- ml_truncated.R
+++ ml_truncated.R
@@ -8,6 +8,36 @@ attach(xm601)
 str(xm601)
 detach(xm601)
 AGE_2<- AGE^2/100
+
+### Panel 1 OLS (p.489)                                                    ###
+y<- log(1+INVEST)[log(1+INVEST)>0]
+eq<- lm(y~GENDER[log(1+INVEST)>0]+ACTIVITY[log(1+INVEST)>0]
+       +AGE[log(1+INVEST)>0]+AGE_2[log(1+INVEST)>0])
+summary(eq)
+Call:
+lm(formula = y ~ GENDER[log(1 + INVEST) > 0] + ACTIVITY[log(1 + 
+    INVEST) > 0] + AGE[log(1 + INVEST) > 0] + AGE_2[log(1 + INVEST) > 
+    0])
+Residuals:
+    Min      1Q  Median      3Q     Max 
+-2.4936 -0.6527 -0.0191  0.6210  3.3399 
+Coefficients:
+                              Estimate Std. Error t value Pr(>|t|)    
+(Intercept)                    2.85424    0.61133   4.669 3.97e-06 ***
+GENDER[log(1 + INVEST) > 0]   -0.21403    0.11579  -1.848  0.06516 .  
+ACTIVITY[log(1 + INVEST) > 0] -0.13212    0.09967  -1.326  0.18564    
+AGE[log(1 + INVEST) > 0]       0.06978    0.02490   2.802  0.00529 ** 
+AGE_2[log(1 + INVEST) > 0]    -0.05593    0.02416  -2.315  0.02103 *  
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+Residual standard error: 0.9443 on 465 degrees of freedom
+  (30 observations deleted due to missingness)
+Multiple R-squared:  0.04784,   Adjusted R-squared:  0.03965 
+F-statistic: 5.841 on 4 and 465 DF,  p-value: 0.0001361
+
+
+
+
+
 library(maxLik)
 ### log(L)= -n/2*log(2*pi)-n/2*log(sigma^2)-1/(2*sigma^2)*sum(y-mu)^2
 ###            -sum(log(pnorm(mu)/sigma))
@@ -20,16 +50,21 @@ beta3<- beta[3]
 beta4<- beta[4]
 beta5<- beta[5]
 sigma<- beta[6]
-y<- LOGINV[LOGINV>0]
-n<- 500
-mu<- beta1+beta2*GENDER+beta3*ACTIVITY+beta4*AGE+beta5*AGE_2
--n/2*log(2*pi)-n/2*log(sigma^2)-1/(2*sigma^2)*sum((y-mu)^2)
-            -sum(log(pnorm(mu,0,1)))
+y<- log(1+INVEST)
+y1<- y[INVEST>0]
+n<- 470
+mu<- beta1+beta2*GENDER[log(1 + INVEST) > 0]+beta3*ACTIVITY[log(1 + INVEST) > 0]
+     +beta4*AGE[log(1 + INVEST) > 0]+beta5*AGE_2[log(1 + INVEST) > 0]
+
+-n/2*log(2*pi) -n/2*log(sigma^2) -1/(2*sigma^2)*sum((y1-mu)^2)
+            -sum(log(pnorm(mu/sigma,0,1)))
 }
-m<- maxLik(l, start= c(0,1,0,0,0,0))
-summary(m)????
+m<- maxLik(l, start= c(0,1,1,0,0,1))
+summary(m)
+
+
+
 library(truncreg)
-y<- LOGINV
 d<- xm601
 yt <- ifelse(y> 1, y, NA)
 AGE_2<- AGE^2
