ef6df6c6f97c86eea777ac51ba085ac446930e99
sadamich <sadamichikato@gmail.com>
2026-08-13 14:48:57 +0900

    Strike duration

--- survival_descriptive.R
+++ survival_descriptive.R
@@ -28,3 +28,25 @@ int [1:62] 1 2 2 2 3 3 3 3 3 4 ...
 quan<- c(x1,x2,x3,x4)
 q<- c(16,16,14,16)
 plot(q)
+
+
+### R. Hatzinger, K. Hornik, H. Nagel, M.J.Maier (2014), R Einführung durch ###
+### angewandte Statistik, Pearson                                           ###
+### Quelle: https://www.pearson.de/r-9783868942507                          ###
+### Seite 306
+
+### Quantile Seite 313
+minimum <- min(x)
+quartil_1<- quantile(x, 0.25)
+quartil_3<- quantile(x, 0.75)
+maximum<- max(x)
+rbind(minimum, quartil_1, quartil_3, maximum)
+          25%
+minimum     1.00
+quartil_1  10.25
+quartil_3  51.25
+maximum   216.00
+
+### Boxplot Seite 317 
+boxplot(x, ylab= "Strike duration in days")
+
