e54f1efdda49816962f2f5bd09994c8b0da54992
sadamich <sadamichikato@gmail.com>
2026-08-30 04:58:17 +0900

    F test

---
+++ theory_test2.R
@@ -0,0 +1,15 @@
+### R. Hatzinger, K. Hornik, H. Nagel, M.J.Maier (2014), R Einführung durch ###
+### angewandte Statistik, Pearson                                           ###
+### Quelle: https://www.pearson.de/r-9783868942507                          ###
+### Seite 365,369,410,
+
+### Source:Christiaan Heij, Paul de Boer, Philip Hans Franses, Teun Kloek, ###
+### Herman K. van Dijk (2004).Econometric Methods with Applications in     ###
+### Business and Economics. Oxford University Press                        ###
+### https://global.oup.com/booksites/content/0199268010/                   ###
+### p. 62 
+### The F test
+### From the chisq and the chisq to the F test
+
+
+
