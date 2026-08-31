041db86873a82383e1f944fdf3430df5407eae97
sadamich <sadamichikato@gmail.com>
2026-08-31 15:46:22 +0900

    Uniform distribution

--- distribution_uniform.R
+++ distribution_uniform.R
@@ -6,6 +6,32 @@ punif(q, min = 0, max = 1, lower.tail = TRUE, log.p = FALSE)
 qunif(p, min = 0, max = 1, lower.tail = TRUE, log.p = FALSE)
 runif(n, min = 0, max = 1)
 
+set.seed(27)
+u <- runif(20)
+u
+[1] 0.971750231 0.083757509 0.873869923 0.329231364 0.222275513 0.401648218
+ [7] 0.072498519 0.002450234 0.137093998 0.191909064 0.616095188 0.849617501
+[13] 0.502747068 0.738322620 0.876447730 0.786295214 0.983604702 0.090687865
+[19] 0.593999193 0.046066507
+plot(u)
+hist(u)
+punif(u)
+[1] 0.971750231 0.083757509 0.873869923 0.329231364 0.222275513 0.401648218
+ [7] 0.072498519 0.002450234 0.137093998 0.191909064 0.616095188 0.849617501
+[13] 0.502747068 0.738322620 0.876447730 0.786295214 0.983604702 0.090687865
+[19] 0.593999193 0.046066507
+dunif(u)
+[1] 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+## The following relations always hold :
+punif(u) == u
+dunif(u) == 1
+
+var(runif(10000))  #- ~ = 1/12 = .08333
+
+
+
+
+
 Zum Nachdenken der Anwendbarkeit der Gleichverteilung 
 ### R. Hatzinger, K. Hornik, H. Nagel, M.J.Maier (2014), R Einführung durch ###
 ### angewandte Statistik, Pearson                                           ###
