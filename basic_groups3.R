cf2512b2c70fd72a6610c6425cae3e101db0ebf8
sadamich <sadamichikato@gmail.com>
2026-09-01 15:34:08 +0900

    Bank wages FEM

---
+++ basic_groups3.R
@@ -0,0 +1,33 @@
+### Source:Christiaan Heij, Paul de Boer, Philip Hans Franses, Teun Kloek, ###
+### Herman K. van Dijk (2004).Econometric Methods with Applications in     ###
+### Business and Economics. Oxford University Press                        ###
+### https://global.oup.com/booksites/content/0199268010/                   ###
+
+### R. Hatzinger, K. Hornik, H. Nagel, M.J.Maier (2014), R Einführung durch ###
+### angewandte Statistik, Pearson                                           ###
+### Quelle: https://www.pearson.de/r-9783868942507                          ###
+xm301<- read.csv("xm301.csv",header=TRUE)
+attach(xm301)
+str(xm301)
+### Seite 409 
+jobcat<- factor(JOBCAT)
+levels(jobcat)<- c("Admistration","Custdials","management")
+boxplot(LOGSAL~jobcat, xlab="Job categories", ylab="Log Salary")
+### Seite 410
+mws<- tapply(LOGSAL, jobcat, mean)
+sds<- tapply(LOGSAL, jobcat, sd)
+rbind(Mean = mws,Sd= sds)
+ Admistration   Custdials management
+Mean   10.2025355 10.33745249  11.029622
+Sd      0.2458633  0.07000588   0.268648
+
+### Seite 411 
+bartlett.test(LOGSAL, jobcat) 
+  Bartlett test of homogeneity of variances
+data:  LOGSAL and jobcat
+Bartlett's K-squared = 42.388, df = 2, p-value = 6.245e-10
+(keine gleiche Varianzen zwischen 1,2 und 3te Gruppen)F test nicht anwendbar
+
+
+### Seite 417
+boxplot(LOGSAL ~ (jobcat + GENDER), ylab="Log Salary")
