b32fcd0e2083b71f1472af22cd56dce45cc5a9a7
sadamich <sadamichikato@gmail.com>
2026-09-06 13:37:59 +0900

    PCA Students scores

--- principal_micro.R
+++ principal_micro.R
@@ -12,7 +12,8 @@ str(xm608micro)
 attach(xm608micro)
 ### Seite 457 
 micro<- na.omit(xm608micro[ , 2:11])
-itemname<- c("ADVMATH1","ADVMATH2","ADVMATH3","CHEMISTRY","FEMALE",
+micro<- micro[-5]
+itemname<- c("ADVMATH1","ADVMATH2","ADVMATH3","CHEMISTRY",
              "FRESHMAN","GRADEHIGH","GRADELOW","GRADFINTERMICRO",
              "GRINERMICRO")
 colnames(micro)<- itemname
@@ -33,11 +34,12 @@ KMO-Criterion: 0.4371419
 print(kmos,stats ="MSA", sort = TRUE, digits=3)
 Kaiser-Meyer-Olkin Statistics
 Call: KMOS(x = micro)
+
 Measures of Sampling Adequacy (MSA):
        ADVMATH3 GRADFINTERMICRO        FRESHMAN        GRADELOW        ADVMATH1 
-          0.078           0.334           0.378           0.412           0.440 
-       ADVMATH2       GRADEHIGH     GRINERMICRO          FEMALE       CHEMISTRY 
-          0.441           0.473           0.668           0.744           0.792 
+          0.069           0.334           0.359           0.411           0.440 
+       ADVMATH2       GRADEHIGH     GRINERMICRO       CHEMISTRY 
+          0.440           0.471           0.661           0.846 
 
 VSS.scree(micro)
 
@@ -45,3 +47,67 @@ VSS.scree(micro)
 pca.micro<- principal(micro, 5, rotate="none")
 pca.micro$criteria<- NULL
 pca.micro
+pca.micro_r<- principal(micro, 5)
+pca.micro_r$criteria<- NULL
+print(pca.micro_r, cut = 0.5, sort=TRUE, digits=2)
+
+### Seite 462
+fa.diagram(pca.micro_r, cut=0.5, cex=0.8,rsize=0.5, main="")
+
+### Seite 463
+pca.micro2<- principal(micro, 2)
+pca.micro2$criteria<- NULL
+print(pca.micro2, cut = 0.5, sort=TRUE, digits=2)
+
+fa.diagram(pca.micro2, cut=0.5, cex=0.8,rsize=0.5, main="")
+
+
+### Seite 468
+pca.micro<- principal(micro, 5, scores=TRUE)
+head(pca.micro$scores)
+micro.scores<- data.frame(pca.micro$scores)
+names(micro.scores)<- c("Grade","Math","Character","Intermicro","Math3")
+
+
+micro.scores<- data.frame(micro.scores, FEMALE)
+boxplot(Math ~ FEMALE, data = micro.scores)
+describeBy(micro.scores$Math, FEMALE, skew=FALSE)
+Descriptive statistics by group 
+group: 0
+   vars   n mean   sd median   min  max range   se
+X1    1 373 0.01 0.98   0.52 -2.06 1.41  3.47 0.05
+------------------------------------------------------------ 
+group: 1
+   vars   n  mean   sd median   min  max range   se
+X1    1 236 -0.02 1.04   0.61 -2.04 1.16  3.21 0.07
+
+
+boxplot(Math3 ~ FEMALE, data = micro.scores)
+describeBy(micro.scores$Math3, FEMALE, skew=FALSE)
+ Descriptive statistics by group 
+group: 0
+   vars   n mean   sd median  min max range   se
+X1    1 373 0.06 1.17  -0.22 -0.4 9.3  9.71 0.06
+------------------------------------------------------------ 
+group: 1
+   vars   n  mean   sd median   min  max range   se
+X1    1 236 -0.09 0.64  -0.22 -0.41 7.37  7.78 0.04
+
+### Seite 470 : Wilcoxon test
+wilcox.test(Math ~ FEMALE, data = micro.scores)
+  Wilcoxon rank sum test with continuity correction
+
+data:  Math by FEMALE
+W = 43888, p-value = 0.9527
+alternative hypothesis: true location shift is not equal to 0
+
+wilcox.test(Math3 ~ FEMALE, data = micro.scores)
+
+boxplot(Character ~ FEMALE, data = micro.scores)
+wilcox.test(Character ~ FEMALE, data = micro.scores)
+  Wilcoxon rank sum test with continuity correction
+
+data:  Character by FEMALE
+W = 35621, p-value = 7.266e-05
+alternative hypothesis: true location shift is not equal to 0
+
