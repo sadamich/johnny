adfcb553b615cc9359e3371d8d03c0663125209b
sadamich <sadamichikato@gmail.com>
2026-09-10 08:36:39 +0900

    Sin Cos Tan

--- trigonometric_function.R
+++ trigonometric_function.R
@@ -1,5 +1,4 @@
 ### Trigonometric function ###
-
 sin(pi/6)
 cos(pi/6)
 tan(pi/6)
@@ -27,11 +26,30 @@ sin(3/8*pi)
 cos(3/8*pi)
 [1] 0.3826834     ### sin(pi/8) = cos(3/8*pi)
 
+### The inverse of sin/cos/tan
+tan(pi/3)
+[1] 1.732051
+atan(1.732051)
+[1] 1.047198
+pi/3
+[1] 1.047198
+cos(pi/3)
+[1] 0.5
+acos(0.5)
+[1] 1.047198
+sin(pi/3)
+[1] 0.8660254
+asin(0.8660254)
+[1] 1.047198
 
+atan2(y, x)
+
+cospi(pi/3)
+[1] -0.9890273
+sinpi(pi/6)
+tanpi(x)
 ### Euler
 exp(*pi) + 1
-
-
 f_euler<- function(x){
 result <- exp(pi*x)+1
 return(result)
