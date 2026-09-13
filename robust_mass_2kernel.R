291264c50b6b3b9d8ab11309e1e45d808755ed09
sadamich <sadamichikato@gmail.com>
2026-09-14 07:09:58 +0900

    Robust estimation

---
+++ robust_mass_2kernel.R
@@ -0,0 +1,33 @@
+https://cran.r-project.org/web/packages/MASS/refman/MASS.html#kde2d
+library(MASS)
+A data frame with 299 observations on 2 variables.
+duration	numeric	Eruption time in mins
+waiting	numeric	Waiting time for this eruption
+str(geyser)
+'data.frame':   299 obs. of  2 variables:
+ $ waiting : num  80 71 57 80 75 77 60 86 77 56 ...
+ $ duration: num  4.02 2.15 4 4 4 ...
+attach(geyser)
+plot(duration, waiting, xlim = c(0.5,6), ylim = c(40,100))
+f1 <- kde2d(duration, waiting, n = 50, lims = c(0.5, 6, 40, 100))
+image(f1, zlim = c(0, 0.05))
+f2 <- kde2d(duration, waiting, n = 50, lims = c(0.5, 6, 40, 100),
+            h = c(width.SJ(duration), width.SJ(waiting)) )
+image(f2, zlim = c(0, 0.05))
+persp(f2, phi = 30, theta = 20, d = 5)
+
+plot(duration[-272], duration[-1], xlim = c(0.5, 6),
+     ylim = c(1, 6),xlab = "previous duration", ylab = "duration")
+f1 <- kde2d(duration[-272], duration[-1],
+            h = rep(1.5, 2), n = 50, lims = c(0.5, 6, 0.5, 6))
+contour(f1, xlab = "previous duration",
+        ylab = "duration", levels  =  c(0.05, 0.1, 0.2, 0.4) )
+f1 <- kde2d(duration[-272], duration[-1],
+            h = rep(0.6, 2), n = 50, lims = c(0.5, 6, 0.5, 6))
+contour(f1, xlab = "previous duration",
+        ylab = "duration", levels  =  c(0.05, 0.1, 0.2, 0.4) )
+f1 <- kde2d(duration[-272], duration[-1],
+            h = rep(0.4, 2), n = 50, lims = c(0.5, 6, 0.5, 6))
+contour(f1, xlab = "previous duration",
+        ylab = "duration", levels  =  c(0.05, 0.1, 0.2, 0.4) )
+detach("geyser")
