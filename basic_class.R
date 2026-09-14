d710e3527330360d7210f8f83c2ff71f8cf3fdf2
sadamich <sadamichikato@gmail.com>
2026-09-15 08:30:33 +0900

    Class

---
+++ basic_class.R
@@ -0,0 +1,19 @@
+x <- 10
+class(x) # "numeric"
+oldClass(x) # NULL
+inherits(x, "a") #FALSE
+class(x) <- c("a", "b")
+inherits(x,"a") #TRUE
+inherits(x, "a", TRUE) # 1
+inherits(x, c("a", "b", "c"), TRUE) # 1 2 0
+
+class( quote(pi) )           # "name"
+## regular calls
+class( quote(sin(pi*x)) )    # "call"
+## special calls
+class( quote(x <- 1) )       # "<-"
+class( quote((1 < 2)) )      # "("
+class( quote( if(8<3) pi ) ) # "if"
+
+.class2(pi)               # "double" "numeric"
+.class2(matrix(1:6, 2,3)) # "matrix" "array" "integer" "numeric"
