a7a104f25014d04faebee2bb0f467b5cacbcc875
sadamich <sadamichikato@gmail.com>
2026-09-18 11:32:23 +0900

    Non Parametric Kern

--- non_parametric.R
+++ non_parametric.R
@@ -23,6 +23,38 @@ w(1)
 w(xbw)
 curve(w, 0, 1, lty=2, ylim=c(0, 1), xlab = "d", ylab="Weight")
 
+# Demonstrate a simple trading strategy for the
+## financial time series German stock index DAX.
+x <- EuStockMarkets[,1]
+k1 <- kernel("daniell", 50)  # a long moving average
+k2 <- kernel("daniell", 10)  # and a short one
+plot(k1)
+plot(k2)
+x1 <- kernapply(x, k1)
+x2 <- kernapply(x, k2)
+plot(x)
+lines(x1, col = "red")    # go long if the short crosses the long upwards
+lines(x2, col = "green")  # and go short otherwise
+
+## More interesting kernels
+kd <- kernel("daniell", c(3, 3))
+kd # note the unusual indexing
+kd[-2:2]
+plot(kernel("fejer", 100, r = 6))
+plot(kernel("modified.daniell", c(7,5,3)))
+
+# Reproduce example 10.4.3 from Brockwell and Davis (1991)
+spectrum(sunspot.year, kernel = kernel("daniell", c(11,7,3)), log = "no")
+
+
+
+
+
+
+
+
+
+
 ### Example 5 3 (p. 293)                                                   ###
 set.seed(35)
 x<- runif(200, 0, 2.5)
@@ -49,9 +81,6 @@ plot(xbw,ybw)
 data_bw<- data.frame(xbw, ybw)
 eq_locfit<- locfit(ybw ~ xbw, data=data_bw)
 plot(eq_locfit)
-
-
-
 eq_w<- lm(y_w~x_w)
 summary(eq_w)
 const<- 0.682819 
