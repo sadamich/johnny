988a0be2ba160d7424de387e13eeac2fb638f85e
sadamich <sadamichikato@gmail.com>
2026-09-19 06:24:03 +0900

    Finance stock markets

---
+++ tseries_finance_markets.R
@@ -0,0 +1,32 @@
+### S&P 500 : The time series
+con <- url("https://finance.yahoo.com")
+if(!inherits(try(open(con), silent = TRUE), "try-error")) {
+  close(con)
+nDays <- 50
+  instrument <- "^gspc"
+  start <- strftime(as.POSIXlt(Sys.time() - nDays * 24 * 3600),
+                    format="%Y-%m-%d") 
+  end <- strftime(as.POSIXlt(Sys.time()), format = "%Y-%m-%d") 
+  x <- get.hist.quote(instrument = instrument, start = start, end = end,
+                      retclass = "ts")
+
+  plotOHLC(x, ylab = "price", main = instrument)
+}
+
+### SBUX
+con <- url("https://finance.yahoo.com")
+if(!inherits(try(open(con), silent = TRUE), "try-error")) {
+  close(con)
+nDays <- 50
+  instrument <- "SBUX"
+  start <- strftime(as.POSIXlt(Sys.time() - nDays * 24 * 3600),
+                    format="%Y-%m-%d") 
+  end <- strftime(as.POSIXlt(Sys.time()), format = "%Y-%m-%d") 
+  x <- get.hist.quote(instrument = instrument, start = start, end = end,
+                      retclass = "ts")
+
+  plotOHLC(x, ylab = "price", main = instrument)
+}
+
+
+
