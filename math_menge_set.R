b4c9b5b6d5746cd4ed2169b92203d2676ddd047b
sadamich <sadamichikato@gmail.com>
2026-09-20 09:26:37 +0900

    Set theory

--- math_menge_set.R
+++ math_menge_set.R
@@ -1,3 +1,37 @@
+### R. Hatzinger, K. Hornik, H. Nagel, M.J.Maier (2014), R Einführung durch ###
+### angewandte Statistik, Pearson                                           ###
+### Quelle: https://www.pearson.de/r-9783868942507                          ###
+### Seite 105 Logische Operationen
+
+### Mengentheorie : the set theory
+A<- c(1,2,3,6)
+A== 1
+[1]  TRUE FALSE FALSE FALSE
+6/A==0
+[1] FALSE FALSE FALSE FALSE
+6/A!=0
+[1] TRUE TRUE TRUE TRUE
+
+B<- 2:20
+B<=20
+ [1] TRUE TRUE TRUE TRUE TRUE TRUE TRUE TRUE TRUE TRUE TRUE TRUE TRUE TRUE TRUE
+[16] TRUE TRUE TRUE TRUE
+C<- Inf
+C==0
+
+D<- A+B
+
+D <= 20
+A==B
+
+A<=B
+
+A | B
+
+A >= B
+
+!B
+
 
 
 
@@ -5,14 +39,7 @@
 
 
 https://de.wikipedia.org/wiki/Abgeschlossene_Menge
-
-
-
 https://de.wikipedia.org/wiki/Beschr%C3%A4nkte_Menge
-
-
 https://de.wikipedia.org/wiki/Offene_Menge
-
-
 Einheitsintervall 
 [0,1]⊂R{\displaystyle [0,1]\subset \mathbb {R} } 
