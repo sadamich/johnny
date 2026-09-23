fd5d8f7a77e1c8feb1aceb2ee00a40486aeacda5
sadamich <sadamichikato@gmail.com>
2026-09-24 06:03:25 +0900

    US interest

---
+++ finance_interest_us.R
@@ -0,0 +1,12 @@
+https://cran.r-project.org/web/packages/tseries/refman/tseries.html#tcm
+https://www.federalreserve.gov/Releases/H15/
+library(tseries)
+
+data(tcm)
+str(tcm)
+
+
+ Time-Series [1:558, 1:4] from 1953 to 2000: 2.36 2.48 2.45 2.38 2.28 2.2 1.79 1.67 1.66 1.41 ...
+ - attr(*, "dimnames")=List of 2
+  ..$ : NULL
+  ..$ : chr [1:4] "tcm1y" "tcm3y" "tcm5y" "tcm10y"
