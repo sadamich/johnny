0ac0b498bf2a570d50c74d22d49dd65e4e12bfcf
sadamich <sadamichikato@gmail.com>
2026-09-26 07:34:33 +0900

    MASS Kernel

--- robust_mass_2kernel.R
+++ robust_mass_2kernel.R
@@ -8,9 +8,12 @@ str(geyser)
  $ waiting : num  80 71 57 80 75 77 60 86 77 56 ...
  $ duration: num  4.02 2.15 4 4 4 ...
 attach(geyser)
+hist(waiting)
+hist(duration)
 plot(duration, waiting, xlim = c(0.5,6), ylim = c(40,100))
 f1 <- kde2d(duration, waiting, n = 50, lims = c(0.5, 6, 40, 100))
 image(f1, zlim = c(0, 0.05))
+summary(f1)
 f2 <- kde2d(duration, waiting, n = 50, lims = c(0.5, 6, 40, 100),
             h = c(width.SJ(duration), width.SJ(waiting)) )
 image(f2, zlim = c(0, 0.05))
